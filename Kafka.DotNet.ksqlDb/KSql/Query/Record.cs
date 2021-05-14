1695d2c1ce8f8e4ea9681a2c18779675ae0d3c5b
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-05-14 16:43:46 +0200

    [ksqldb] - Record.RowTime was decorated with IgnoreAttribute

--- Kafka.DotNet.ksqlDb/KSql/Query/Record.cs
+++ Kafka.DotNet.ksqlDb/KSql/Query/Record.cs
@@ -1,7 +1,10 @@
-﻿namespace Kafka.DotNet.ksqlDB.KSql.Query
+﻿using Kafka.DotNet.ksqlDB.KSql.RestApi.Statements.Annotations;
+
+namespace Kafka.DotNet.ksqlDB.KSql.Query
 {
   public class Record
   {
+    [Ignore]
     public long RowTime { get; set; }
   }
 }
