4500392495bbce2d2dbbe08fde14bc817993d859
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-05 14:16:25 +0200

    [Kafka.DotNet.ksqlDB.InsideOut] - serdes

---
+++ Kafka.DotNet.ksqlDB.InsideOut/Serdes/KafkaJsonDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace Kafka.DotNet.ksqlDB.InsideOut.Serdes
+{
+  public class KafkaJsonDeserializer<TValue> : IDeserializer<TValue>
+  {
+    public TValue Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {      
+      if (isNull)
+        return default;
+
+      byte[] dataBytes = data.ToArray();
+
+      TValue result = new DataContractJsonDeserializer<TValue>().Deserialize(dataBytes);
+
+      return result;
+    }
+  }
+}
---
+++ Kafka.DotNet.ksqlDB.InsideOut/Serdes/KafkaJsonSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using Confluent.Kafka;
+
+namespace Kafka.DotNet.ksqlDB.InsideOut.Serdes
+{
+  public class KafkaJsonSerializer<TValue> : ISerializer<TValue>
+  {
+    public byte[] Serialize(TValue data, SerializationContext context)
+    {
+      var serializer = new DataContractJsonSerializer(typeof(TValue));
+
+      using var memoryStream = new MemoryStream();
+      
+      serializer.WriteObject(memoryStream, data);
+
+      return memoryStream.ToArray();
+    }
+  }
+}
