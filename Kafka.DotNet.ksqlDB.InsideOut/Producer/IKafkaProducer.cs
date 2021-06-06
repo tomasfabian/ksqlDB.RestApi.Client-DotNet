98e6f29ee44ef0862564842b008f8c2dfd59c70b
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-06 14:27:07 +0200

    [Db.InsideOut] - IKafkaProducer<TKey, TValue>

---
+++ Kafka.DotNet.ksqlDB.InsideOut/Producer/IKafkaProducer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Kafka.DotNet.ksqlDB.InsideOut.Producer
+{
+  public interface IKafkaProducer : IDisposable
+  {
+    string TopicName { get; }
+  }
+
+  public interface IKafkaProducer<TKey, TValue> : IKafkaProducer
+  {
+    Task<DeliveryResult<TKey, TValue>> ProduceMessageAsync(TKey key, TValue value);
+    Task<DeliveryResult<TKey, TValue>> DeleteMessageAsync(TKey id);
+  }
+}
