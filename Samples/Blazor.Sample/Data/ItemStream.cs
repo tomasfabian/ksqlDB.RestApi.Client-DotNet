8cbc89f592518dd7ba64f343c9000329475866ef
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-07 19:14:27 +0200

    [Blazor.Sample] - Models

---
+++ Samples/Blazor.Sample/Data/ItemStream.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Blazor.Sample.Data
+{
+  [DataContract]
+  public record ItemStream
+  {
+    public int Id { get; set; }
+
+    [DataMember(Name = "DESCRIPTION")]
+    public string Description { get; set; }
+  }
+}
---
+++ Samples/Blazor.Sample/Data/ItemTable.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Blazor.Sample.Data
+{
+  [DataContract]
+  public record ItemTable
+  {
+    [DataMember(Name = "COUNT")]
+    public int Count { get; set; }
+  }
+}
