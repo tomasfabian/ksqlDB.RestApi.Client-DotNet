c736da94dba17749576f887ceabf490d6b17f374
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-09 15:28:42 +0200

    [Blazor.Sample] - FixedSizedQueue

---
+++ Samples/Blazor.Sample/Collections/FixedSizedQueue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Sample.Collections
+{
+  public class FixedSizedQueue<T> : IEnumerable<T>
+  {
+    readonly ConcurrentQueue<T> queue = new();
+
+    public int Limit { get; set; } = 5;
+
+    public void Enqueue(T obj)
+    {
+      queue.Enqueue(obj);
+
+      while (queue.Count > Limit && queue.TryDequeue(out _))
+      {
+      }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      return queue.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return ((IEnumerable)queue).GetEnumerator();
+    }
+  }
+}
