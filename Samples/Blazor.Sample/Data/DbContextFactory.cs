9df526f607c78a417f43142933cfca3e85e607fd
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-14 16:30:06 +0200

    [Blazor.Sample] - added entity framework sensors migration

---
+++ Samples/Blazor.Sample/Data/DbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Blazor.Sample.Data
+{
+  public class DbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+  {
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+      var configuration = new ConfigurationBuilder()        
+        .Build();
+      
+      var connectionString = configuration["ConnectionStrings:DefaultConnection"];
+
+      var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+      optionsBuilder.UseSqlServer(connectionString);
+
+      return new ApplicationDbContext(optionsBuilder.Options);
+    }
+  }
+}
---
+++ Samples/Blazor.Sample/Migrations/20210614142530_Sensors.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blazor.Sample.Migrations
+{
+    public partial class Sensors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sensors",
+                columns: table => new
+                {
+                    SensorId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensors", x => x.SensorId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sensors");
+        }
+    }
+}
