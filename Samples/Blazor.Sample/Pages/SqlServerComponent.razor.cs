89f94fb66416c97558654f61c17435aadae13358
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-18 16:30:41 +0200

    [Blazor.Sample] - Sql server

---
+++ Samples/Blazor.Sample/Pages/SqlServerComponent.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazor.Sample.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Sample.Pages
+{
+  public partial class SqlServerComponent
+  {
+    [Inject] private ApplicationDbContext DbContext { get; init; }
+
+    protected override async Task OnInitializedAsync()
+    {
+      var sensors = await DbContext.Sensors.ToListAsync();
+
+      await base.OnInitializedAsync();
+    }
+  }
+}
