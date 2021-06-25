7a0b2185e317e4f463f795e5480c6b6fc45d03b6
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-25 16:58:51 +0200

    [kafka.dotnet.SqlServer] - CDC data types

---
+++ Kafka.DotNet.SqlServer/Cdc/DatabaseChangeObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Kafka.DotNet.SqlServer.Cdc
+{
+  public class DatabaseChangeObject
+  {
+    public string Before { get; set; }
+    public string After { get; set; }
+    public string Source { get; set; }
+    
+    public string Op { get; set; }
+  }
+}
