6cc3198d05afca4424467005c1c72588443995f6
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-26 11:06:32 +0200

    [Kafka.DotNet.SqlServer] - connector metadata improvements

--- Kafka.DotNet.SqlServer/Cdc/Connectors/ConnectorMetadata.cs
+++ Kafka.DotNet.SqlServer/Cdc/Connectors/ConnectorMetadata.cs
@@ -1,67 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Data.SqlClient;
+﻿using System.Collections.Generic;
 
 namespace Kafka.DotNet.SqlServer.Cdc.Connectors
 {
   public record ConnectorMetadata
   {
-    public ConnectorMetadata(SqlConnectionStringBuilder sqlConnectionStringBuilder)
-      : this()
-    {
-      if (sqlConnectionStringBuilder == null) throw new ArgumentNullException(nameof(sqlConnectionStringBuilder));
-
-      FillFrom(sqlConnectionStringBuilder);
-    }
-
-    public ConnectorMetadata(string connectionString)
-      : this()
-    {
-      if (connectionString == null)
-        throw new ArgumentNullException(nameof(connectionString));
-
-      if (connectionString.Trim() == String.Empty)
-        throw new ArgumentException($"{nameof(connectionString)} cannot be empty", nameof(connectionString));
-
-      SqlConnectionStringBuilder sqlConnectionStringBuilder = new(connectionString);
-
-      FillFrom(sqlConnectionStringBuilder);
-    }
-
-    private void FillFrom(SqlConnectionStringBuilder sqlConnectionStringBuilder)
-    {
-      var dataSource = sqlConnectionStringBuilder.DataSource.Split(',');
-
-      DatabaseHostname = dataSource[0];
-
-      if (dataSource.Length > 1)
-        DatabasePort = dataSource[1];
-
-      DatabaseUser = sqlConnectionStringBuilder.UserID;
-
-      DatabasePassword = sqlConnectionStringBuilder.Password;
-
-      DatabaseDbname = sqlConnectionStringBuilder.InitialCatalog;
-    }
-
-    public ConnectorMetadata()
-    {
-      ConnectorClass = "io.debezium.connector.sqlserver.SqlServerConnector";
-
-      DatabasePort = "1433";
-    }
-
-    private const string NamePropertyName = "connector.class";
+    private const string NamePropertyName = "name";
     private const string ConnectorClassName = "connector.class";
-    private const string DatabaseHostnameName = "database.hostname";
-    private const string DatabaseHPortName = "database.port";
-    private const string DatabaseUserPropertyName = "database.user";
-    private const string DatabasePasswordName = "database.password";
-    private const string DatabaseDbnamePropertyName = "database.dbname";
-    private const string DatabaseServerNamePropertyName = "database.server.name";
-    private const string KafkaBootstrapServersPropertyName = "database.history.kafka.bootstrap.servers";
-    private const string DatabaseHistoryKafkaTopicPropertyName = "database.history.kafka.topic";
-    private const string TableIncludeListPropertyName = "table.include.list";
     private const string KeyConverterPropertyName = "key.converter";
     private const string ValueConverterPropertyName = "value.converter";
 
@@ -76,61 +20,7 @@ namespace Kafka.DotNet.SqlServer.Cdc.Connectors
       get => this[ConnectorClassName];
       set => this[ConnectorClassName] = value;
     }
-
-    public string DatabaseHostname
-    {
-      get => this[DatabaseHostnameName];
-      set => this[DatabaseHostnameName] = value;
-    }
-
-    public string DatabaseUser
-    {
-      get => this[DatabaseUserPropertyName];
-      set => this[DatabaseUserPropertyName] = value;
-    }
-
-    public string DatabasePort
-    {
-      get => this[DatabaseHPortName];
-      set => this[DatabaseHPortName] = value;
-    }
-
-    public string DatabasePassword
-    {
-      get => this[DatabasePasswordName];
-      set => this[DatabasePasswordName] = value;
-    }
-
-    public string DatabaseDbname
-    {
-      get => this[DatabaseDbnamePropertyName];
-      set => this[DatabaseDbnamePropertyName] = value;
-    }
-
-    public string DatabaseServerName
-    {
-      get => this[DatabaseServerNamePropertyName];
-      set => this[DatabaseServerNamePropertyName] = value;
-    }
-
-    public string KafkaBootstrapServers
-    {
-      get => this[KafkaBootstrapServersPropertyName];
-      set => this[KafkaBootstrapServersPropertyName] = value;
-    }
-
-    public string DatabaseHistoryKafkaTopic
-    {
-      get => this[DatabaseHistoryKafkaTopicPropertyName];
-      set => this[DatabaseHistoryKafkaTopicPropertyName] = value;
-    }
-
-    public string TableIncludeList
-    {
-      get => this[TableIncludeListPropertyName];
-      set => this[TableIncludeListPropertyName] = value;
-    }
-
+    
     public string KeyConverter
     {
       get => this[KeyConverterPropertyName];
@@ -174,40 +64,9 @@ namespace Kafka.DotNet.SqlServer.Cdc.Connectors
       return this;
     }
 
-    public ConnectorMetadata SetTableIncludeListPropertyName(string value)
+    private protected bool HasValue(string propertyName)
     {
-      Properties[TableIncludeListPropertyName] = value;
-
-      return this;
-    }
-
-    public bool TrySetConnectorName()
-    {
-      if (HasValue(DatabaseDbnamePropertyName))
-      {
-        Name = $"{DatabaseDbname}-connector";
-
-        return true;
-      }
-
-      return false;
-    }
-
-    public bool TrySetDatabaseHistoryKafkaTopic()
-    {
-      if (HasValue(DatabaseServerNamePropertyName))
-      {
-        DatabaseHistoryKafkaTopic = $"dbhistory.{DatabaseServerName}";
-
-        return true;
-      }
-
-      return false;
-    }
-
-    private bool HasValue(string propertyName)
-    {
-      return Properties.ContainsKey(DatabaseDbname) && !string.IsNullOrEmpty(DatabaseDbname);
+      return Properties.ContainsKey(propertyName) && !string.IsNullOrEmpty(this[propertyName]);
     }
   }
 }
---
+++ Kafka.DotNet.SqlServer/Cdc/Connectors/SqlServerConnectorMetadata.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace Kafka.DotNet.SqlServer.Cdc.Connectors
+{
+  public record SqlServerConnectorMetadata : ConnectorMetadata
+  {
+    public SqlServerConnectorMetadata(SqlConnectionStringBuilder sqlConnectionStringBuilder)
+      : this()
+    {
+      if (sqlConnectionStringBuilder == null) throw new ArgumentNullException(nameof(sqlConnectionStringBuilder));
+
+      FillFrom(sqlConnectionStringBuilder);
+    }
+
+    public SqlServerConnectorMetadata(string connectionString)
+      : this()
+    {
+      if (connectionString == null)
+        throw new ArgumentNullException(nameof(connectionString));
+
+      if (connectionString.Trim() == String.Empty)
+        throw new ArgumentException($"{nameof(connectionString)} cannot be empty", nameof(connectionString));
+
+      SqlConnectionStringBuilder sqlConnectionStringBuilder = new(connectionString);
+
+      FillFrom(sqlConnectionStringBuilder);
+    }
+
+    public SqlServerConnectorMetadata()
+    {
+      ConnectorClass = "io.debezium.connector.sqlserver.SqlServerConnector";
+
+      DatabasePort = "1433";
+    }
+
+    private void FillFrom(SqlConnectionStringBuilder sqlConnectionStringBuilder)
+    {
+      var dataSource = sqlConnectionStringBuilder.DataSource.Split(',');
+
+      DatabaseHostname = dataSource[0];
+
+      if (dataSource.Length > 1)
+        DatabasePort = dataSource[1];
+
+      DatabaseUser = sqlConnectionStringBuilder.UserID;
+
+      DatabasePassword = sqlConnectionStringBuilder.Password;
+
+      DatabaseDbname = sqlConnectionStringBuilder.InitialCatalog;
+    }
+
+    private const string DatabaseHostnameName = "database.hostname";
+    private const string DatabaseHPortName = "database.port";
+    private const string DatabaseUserPropertyName = "database.user";
+    private const string DatabasePasswordName = "database.password";
+    private const string DatabaseDbnamePropertyName = "database.dbname";
+    private const string DatabaseServerNamePropertyName = "database.server.name";
+    private const string KafkaBootstrapServersPropertyName = "database.history.kafka.bootstrap.servers";
+    private const string DatabaseHistoryKafkaTopicPropertyName = "database.history.kafka.topic";
+    private const string TableIncludeListPropertyName = "table.include.list";
+
+    
+    public string DatabaseHostname
+    {
+      get => this[DatabaseHostnameName];
+      set => this[DatabaseHostnameName] = value;
+    }
+
+    public string DatabaseUser
+    {
+      get => this[DatabaseUserPropertyName];
+      set => this[DatabaseUserPropertyName] = value;
+    }
+
+    public string DatabasePort
+    {
+      get => this[DatabaseHPortName];
+      set => this[DatabaseHPortName] = value;
+    }
+
+    public string DatabasePassword
+    {
+      get => this[DatabasePasswordName];
+      set => this[DatabasePasswordName] = value;
+    }
+
+    public string DatabaseDbname
+    {
+      get => this[DatabaseDbnamePropertyName];
+      set => this[DatabaseDbnamePropertyName] = value;
+    }
+
+    public string DatabaseServerName
+    {
+      get => this[DatabaseServerNamePropertyName];
+      set => this[DatabaseServerNamePropertyName] = value;
+    }
+
+    public string KafkaBootstrapServers
+    {
+      get => this[KafkaBootstrapServersPropertyName];
+      set => this[KafkaBootstrapServersPropertyName] = value;
+    }
+
+    public string DatabaseHistoryKafkaTopic
+    {
+      get => this[DatabaseHistoryKafkaTopicPropertyName];
+      set => this[DatabaseHistoryKafkaTopicPropertyName] = value;
+    }
+
+    public string TableIncludeList
+    {
+      get => this[TableIncludeListPropertyName];
+      set => this[TableIncludeListPropertyName] = value;
+    }
+
+    public SqlServerConnectorMetadata SetTableIncludeListPropertyName(string value)
+    {
+      Properties[TableIncludeListPropertyName] = value;
+
+      return this;
+    }
+
+    public bool TrySetConnectorName()
+    {
+      if (!HasValue(DatabaseDbnamePropertyName)) 
+        return false;
+      
+      Name = $"{DatabaseDbname}-connector";
+
+      return true;
+    }
+
+    public bool TrySetDatabaseHistoryKafkaTopic()
+    {
+      if (!HasValue(DatabaseServerNamePropertyName)) 
+        return false;
+
+      DatabaseHistoryKafkaTopic = $"dbhistory.{DatabaseServerName}";
+
+      return true;
+    }
+  }
+}
