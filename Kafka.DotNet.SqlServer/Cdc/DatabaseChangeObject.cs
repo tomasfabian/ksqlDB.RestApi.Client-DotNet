eacd11c5309ccde6b43cd3bcd6ac59ecd9f4cf96
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-06-30 16:22:17 +0200

    [Kafka.DotNet.SqlServer] - DatabaseChangeObject renamed to RawDatabaseChangeObject

--- Kafka.DotNet.SqlServer/Cdc/DatabaseChangeObject.cs
+++ Kafka.DotNet.SqlServer/Cdc/DatabaseChangeObject.cs
@@ -1,45 +1,12 @@
-﻿using Kafka.DotNet.SqlServer.Cdc.Extensions;
-
-namespace Kafka.DotNet.SqlServer.Cdc
+﻿namespace Kafka.DotNet.SqlServer.Cdc
 {
-  public record DatabaseChangeObject<TEntity> : DatabaseChangeObject
+  public record DatabaseChangeObject<TEntity>
   {
-    public TEntity EntityBefore
-    {
-      get
-      {
-        var entity = DeserializeValue(Before);
-
-        return entity;
-      }
-    }
-
-    public TEntity EntityAfter
-    {
-      get
-      {
-        var entity = DeserializeValue(After);
-
-        return entity;
-      }
-    }
-
-    protected virtual TEntity DeserializeValue(string value)
-    {
-      var entity = System.Text.Json.JsonSerializer.Deserialize<TEntity>(value);
-
-      return entity;
-    }
-  }
-
-  public record DatabaseChangeObject
-  {
-    public string Before { get; set; }
-    public string After { get; set; }
-    public string Source { get; set; }
-    
+    public TEntity Before { get; set; }
+    public TEntity After { get; set; }
+    public Source Source { get; set; }
     public string Op { get; set; }
-
-    public ChangeDataCaptureType OperationType => Op.ToChangeDataCaptureType();
+    public long TsMs { get; set; }
+    public object Transaction { get; set; }
   }
 }
---
+++ Kafka.DotNet.SqlServer/Cdc/RawDatabaseChangeObject.cs
@@ -0,0 +1,45 @@
+﻿using Kafka.DotNet.SqlServer.Cdc.Extensions;
+
+namespace Kafka.DotNet.SqlServer.Cdc
+{
+  public record RawDatabaseChangeObject
+  {
+    public string Before { get; set; }
+    public string After { get; set; }
+    public string Source { get; set; }
+    
+    public string Op { get; set; }
+
+    public ChangeDataCaptureType OperationType => Op.ToChangeDataCaptureType();
+  }
+
+  public record RawDatabaseChangeObject<TEntity> : RawDatabaseChangeObject
+  {
+    public TEntity EntityBefore
+    {
+      get
+      {
+        var entity = DeserializeValue(Before);
+
+        return entity;
+      }
+    }
+
+    public TEntity EntityAfter
+    {
+      get
+      {
+        var entity = DeserializeValue(After);
+
+        return entity;
+      }
+    }
+
+    protected virtual TEntity DeserializeValue(string value)
+    {
+      var entity = System.Text.Json.JsonSerializer.Deserialize<TEntity>(value);
+
+      return entity;
+    }
+  }
+}
