d48e50b0a92337a7677ef8e2e442c1bf8f3fe7e7
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-07-01 16:42:31 +0200

    [Kafka.DotNet.SqlServer] - ConnectorMetadataTests

---
+++ Tests/Kafka.DotNet.SqlServer.Tests/Cdc/Connectors/ConnectorMetadataTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Kafka.DotNet.SqlServer.Cdc.Connectors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests;
+
+namespace Kafka.DotNet.SqlServer.Tests.Cdc.Connectors
+{
+  [TestClass]
+  public class ConnectorMetadataTests : TestBase<ConnectorMetadata>
+  {    
+    [TestInitialize]
+    public override void TestInitialize()
+    {
+      base.TestInitialize();
+
+      ClassUnderTest = new ConnectorMetadata();
+    }
+
+    [TestMethod]
+    public void JsonKeyConverter()
+    {
+      //Arrange
+
+      //Act
+      ClassUnderTest.SetJsonKeyConverter();
+
+      //Assert
+      ClassUnderTest.KeyConverter.Should().Be("org.apache.kafka.connect.json.JsonConverter");
+    }
+
+    [TestMethod]
+    public void ValueConverter()
+    {
+      //Arrange
+
+      //Act
+      ClassUnderTest.SetJsonValueConverter();
+
+      //Assert
+      ClassUnderTest.ValueConverter.Should().Be("org.apache.kafka.connect.json.JsonConverter");
+    }
+  }
+}
