8f3e5487b20d9a95e11f1b66d158c6f19d78e4d7
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-07-08 07:12:54 +0200

    [Kafka.DotNet.SqlServer] - OperationTypeExtensions Tests

---
+++ Tests/Kafka.DotNet.SqlServer.Tests/Cdc/Extensions/OperationTypeExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Kafka.DotNet.SqlServer.Cdc;
+using Kafka.DotNet.SqlServer.Cdc.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests;
+
+namespace Kafka.DotNet.SqlServer.Tests.Cdc.Extensions
+{
+  [TestClass]
+  public class OperationTypeExtensionsTests : TestBase
+  {
+    [TestMethod]
+    public void ToChangeDataCaptureType_r_Read()
+    {
+      //Arrange
+      string operation = "r";
+
+      //Act
+      var cdcTpe = operation.ToChangeDataCaptureType();
+
+      //Assert
+      cdcTpe.Should().Be(ChangeDataCaptureType.Read);
+    }
+
+    [TestMethod]
+    public void ToChangeDataCaptureType_c_Created()
+    {
+      //Arrange
+      string operation = "c";
+
+      //Act
+      var cdcTpe = operation.ToChangeDataCaptureType();
+
+      //Assert
+      cdcTpe.Should().Be(ChangeDataCaptureType.Created);
+    }
+
+    [TestMethod]
+    public void ToChangeDataCaptureType_u_Updated()
+    {
+      //Arrange
+      string operation = "u";
+
+      //Act
+      var cdcTpe = operation.ToChangeDataCaptureType();
+
+      //Assert
+      cdcTpe.Should().Be(ChangeDataCaptureType.Updated);
+    }
+
+    [TestMethod]
+    public void ToChangeDataCaptureType_d_Deleted()
+    {
+      //Arrange
+      string operation = "d";
+
+      //Act
+      var cdcTpe = operation.ToChangeDataCaptureType();
+
+      //Assert
+      cdcTpe.Should().Be(ChangeDataCaptureType.Deleted);
+    }
+  }
+}
