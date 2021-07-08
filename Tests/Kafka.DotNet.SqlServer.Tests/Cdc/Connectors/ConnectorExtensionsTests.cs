4c36e905e681ef38ac08502685109083f97f86dd
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-07-08 07:12:26 +0200

    [Kafka.DotNet.SqlServer] - ConnectorExtensions Test

---
+++ Tests/Kafka.DotNet.SqlServer.Tests/Cdc/Connectors/ConnectorExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Kafka.DotNet.SqlServer.Cdc.Connectors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests;
+
+namespace Kafka.DotNet.SqlServer.Tests.Cdc.Connectors
+{
+  [TestClass]
+  public class ConnectorExtensionsTests : TestBase
+  {
+    private SqlServerConnectorMetadata CreateConnector()
+    {
+      string connectionString =
+        "Server=127.0.0.1,1433;User Id = SA;Password=<YourNewStrong@Passw0rd>;Initial Catalog = Sensors;MultipleActiveResultSets=true";
+
+      return new SqlServerConnectorMetadata(connectionString);
+    }
+
+    [TestMethod]
+    public void ToStatement()
+    {
+      //Arrange
+      var connector = CreateConnector();
+      string connectorName = "myConnector";
+
+      //Act
+      var statement = connector.ToStatement(connectorName);
+
+      //Assert
+      statement.Should().Be(@$"CREATE SOURCE CONNECTOR {connectorName} WITH (
+	'connector.class'= 'io.debezium.connector.sqlserver.SqlServerConnector', 
+	'database.port'= '1433', 
+	'database.hostname'= '127.0.0.1', 
+	'database.user'= 'SA', 
+	'database.password'= '<YourNewStrong@Passw0rd>', 
+	'database.dbname'= 'Sensors'
+);
+");
+    }
+  }
+}
