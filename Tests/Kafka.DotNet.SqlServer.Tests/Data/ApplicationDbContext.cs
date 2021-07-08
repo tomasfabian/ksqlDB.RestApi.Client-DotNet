57989b09cd7213e268e1fb33ab2d9e931186ed47
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-07-08 16:31:53 +0200

    [Kafka.DotNet.SqlServer] - integration tests db context

---
+++ Tests/Kafka.DotNet.SqlServer.Tests/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kafka.DotNet.SqlServer.Tests.Data
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public DbSet<IoTSensor> Sensors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      base.OnConfiguring(optionsBuilder);
+      
+      string connectionString =
+        "Server=127.0.0.1,1433;User Id = SA;Password=<YourNewStrong@Passw0rd>;Initial Catalog = TestSensors;MultipleActiveResultSets=true";
+
+      optionsBuilder.UseSqlServer(connectionString);
+    }
+  }
+}
---
+++ Tests/Kafka.DotNet.SqlServer.Tests/Data/IoTSensor.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kafka.DotNet.SqlServer.Tests.Data
+{
+  public record IoTSensor
+  {
+    [Key]
+    public string SensorId { get; set; }
+    public int Value { get; set; }
+  }
+}
