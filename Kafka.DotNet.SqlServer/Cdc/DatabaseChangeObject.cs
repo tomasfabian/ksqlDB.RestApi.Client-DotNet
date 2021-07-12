f991209ea2531c447cc9bd2dda28b46a30fb60c5
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-07-12 08:14:01 +0200

    [Kafka.DotNet.SqlServer] - DatabaseChangeObject

--- Kafka.DotNet.SqlServer/Cdc/DatabaseChangeObject.cs
+++ Kafka.DotNet.SqlServer/Cdc/DatabaseChangeObject.cs
@@ -1,12 +1,20 @@
-﻿namespace Kafka.DotNet.SqlServer.Cdc
+﻿using Kafka.DotNet.SqlServer.Cdc.Extensions;
+
+namespace Kafka.DotNet.SqlServer.Cdc
 {
-  public record DatabaseChangeObject<TEntity>
+  public record DatabaseChangeObject<TEntity> : DatabaseChangeObject
   {
     public TEntity Before { get; set; }
     public TEntity After { get; set; }
+  }
+
+  public record DatabaseChangeObject
+  {
     public Source Source { get; set; }
     public string Op { get; set; }
     public long TsMs { get; set; }
     public object Transaction { get; set; }
+    
+    public ChangeDataCaptureType OperationType => Op.ToChangeDataCaptureType();
   }
 }
