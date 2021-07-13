d4eb51e1b7eff12917fc0f68251199269a445c8f
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-07-13 08:00:21 +0200

    [Kafka.DotNet.SqlServer] - ConnectorsResponse

---
+++ Kafka.DotNet.ksqlDb/KSql/RestApi/Connectors/Connector.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Kafka.DotNet.ksqlDB.KSql.RestApi.Connectors
+{
+  public class Connector
+  {
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+    
+    [JsonPropertyName("className")]
+    public string ClassName { get; set; }
+    
+    [JsonPropertyName("state")]
+    public string State { get; set; }
+  }
+
+  internal class ListConnectorsList
+  {
+    public ConnectorsResponse[] Connectors { get; set; }
+  }
+}
---
+++ Kafka.DotNet.ksqlDb/KSql/RestApi/Connectors/ConnectorsResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Kafka.DotNet.ksqlDB.KSql.RestApi.Connectors
+{
+  public class ConnectorsResponse
+  {
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+    
+    [JsonPropertyName("statementText")]
+    public string StatementText { get; set; }
+    
+    [JsonPropertyName("warnings")]
+    public object[] Warnings { get; set; }
+    
+    [JsonPropertyName("connectors")]
+    public Connector[] Connectors { get; set; }
+  }
+}
