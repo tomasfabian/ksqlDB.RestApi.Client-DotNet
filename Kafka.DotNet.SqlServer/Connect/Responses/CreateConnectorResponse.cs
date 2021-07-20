e47163680fb73e7f5f5eb0d192b4c938cb6473a9
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-07-20 20:52:25 +0200

    [Kafka.DotNet.*SqlServer] - CreateConnectorResponse

---
+++ Kafka.DotNet.SqlServer/Connect/Responses/CreateConnectorResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Kafka.DotNet.SqlServer.Connect.Responses
+{
+  public record CreateConnectorResponse
+  {
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("config")]
+    public IDictionary<string, string> Config { get; set; }
+
+    [JsonPropertyName("tasks")]
+    public ConnectorTask[] Tasks { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+  }
+}
