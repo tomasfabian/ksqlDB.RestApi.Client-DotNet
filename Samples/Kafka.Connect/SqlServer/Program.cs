1f9b8f589dbd2fb6645a84fe0a4182f7d9e00738
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-07-21 16:04:08 +0200

    [Kafka.DotNet.SqlServer] - kafka connect example - post connector

--- Samples/Kafka.Connect/SqlServer/Program.cs
+++ Samples/Kafka.Connect/SqlServer/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Kafka.DotNet.ksqlDB.KSql.RestApi;
+using Kafka.DotNet.SqlServer.Cdc.Connectors;
+using Kafka.DotNet.SqlServer.Connect;
 
 namespace Connect.SqlServer
 {
-  class Program
+  public static class Program
   {
-    static void Main(string[] args)
+    static string ConnectUrl => @"http://connect:8083";
+
+    static async Task Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      SqlServerConnectorMetadata connectorMetadata = CreateConnectorMetadata();
+
+      string connectorName = "MSSQL_CDC_CONNECTOR";
+	
+      var httpClientFactory = new HttpClientFactory(new Uri(ConnectUrl));
+	
+      var connectRestApiClient = new ConnectRestApiClient(httpClientFactory);
+	
+      var httpResponseMessage = await connectRestApiClient.PostConnectorAsync(connectorMetadata, connectorName);
+    }
+
+    static string connectionString = @"Server=sqlserver2019,1433;User Id = SA;Password=<YourNewStrong@Passw0rd>;Initial Catalog = Sensors;MultipleActiveResultSets=true";
+
+    static string bootstrapServers = "localhost:29092";
+
+    static string tableName = "Sensors";
+    static string schemaName = "dbo";
+    private static SqlServerConnectorMetadata CreateConnectorMetadata()
+    {
+      var createConnector = new SqlServerConnectorMetadata(connectionString)
+        .SetTableIncludeListPropertyName($"{schemaName}.{tableName}")
+        .SetJsonKeyConverter()
+        .SetJsonValueConverter()
+        .SetProperty("database.history.kafka.bootstrap.servers", bootstrapServers)
+        .SetProperty("database.history.kafka.topic", $"dbhistory.{tableName}")
+        .SetProperty("database.server.name", "sqlserver2019")
+        .SetProperty("key.converter.schemas.enable", "false")
+        .SetProperty("value.converter.schemas.enable", "false")
+        .SetProperty("include.schema.changes", "false");
+
+      return createConnector as SqlServerConnectorMetadata;
     }
   }
 }
