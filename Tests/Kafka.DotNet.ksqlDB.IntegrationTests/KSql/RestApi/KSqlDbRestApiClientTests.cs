e90d00137c7e7c33abe6f956a3053dc45f88518c
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-08-03 07:04:58 +0200

    [Kafka.DotNet.ksqlDB] - TerminatePersistentQueryAsync and create connector  tests

--- Tests/Kafka.DotNet.ksqlDB.IntegrationTests/KSql/RestApi/KSqlDbRestApiClientTests.cs
+++ Tests/Kafka.DotNet.ksqlDB.IntegrationTests/KSql/RestApi/KSqlDbRestApiClientTests.cs
@@ -7,6 +7,8 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Kafka.DotNet.ksqlDB.KSql.Linq.Statements;
+using Kafka.DotNet.ksqlDB.KSql.Query.Context;
 using Kafka.DotNet.ksqlDB.KSql.RestApi;
 using Kafka.DotNet.ksqlDB.KSql.RestApi.Enums;
 using Kafka.DotNet.ksqlDB.KSql.RestApi.Extensions;
@@ -31,6 +33,8 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       restApiClient = new KSqlDbRestApiClient(httpClientFactory);
     }
 
+    private string SuccessStatus => "SUCCESS";
+
     [TestMethod]
     public async Task ExecuteStatementAsync()
     {
@@ -46,7 +50,7 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       string responseContent = await httpResponseMessage.Content.ReadAsStringAsync();
       var responseObject = JsonSerializer.Deserialize<StatementResponse[]>(responseContent);
 
-      responseObject?[0].CommandStatus.Status.Should().Be("SUCCESS");
+      responseObject?[0].CommandStatus.Status.Should().Be(SuccessStatus);
       responseObject?[0].CommandStatus.Message.Should().Be("Table created");
     }
 
@@ -64,7 +68,7 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
 
       HttpResponseMessage httpResponseMessage = await httpResponseMessageTask;
     }
-    
+
     private string CreateTableStatement(string tableName = "TestTable")
     {
       return $@"CREATE OR REPLACE TABLE {tableName} (
@@ -130,31 +134,50 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       responseObject?[0].CommandStatus.Message.Should().Be("Stream created");
     }
 
-    private string ConnectorName => "mock-connector";
+    #region Connectors
+
+    private string SinkConnectorName => "mock-sink-connector";
 
     [TestMethod]
-    public async Task GetConnectorAsync()
+    public async Task CreateSinkConnectorAsync()
     {
       //Arrange
-      var createConnector = @$"CREATE SOURCE CONNECTOR `{ConnectorName}` WITH(
-      'connector.class'='org.apache.kafka.connect.tools.MockSourceConnector',
-      'topic.prefix'='mock-',
-      'table.whitelist'='users',
-      'key'='username');";
-
-      var statement = new KSqlDbStatement(createConnector);
+      var connectorConfig = new Dictionary<string, string> {
+        { "connector.class", "org.apache.kafka.connect.tools.MockSinkConnector" },
+        { "topics.regex", "mock-sink*"},
+      };
 
       //Act
-      var httpResponseMessage = await restApiClient.ExecuteStatementAsync(statement);
+      var httpResponseMessage = await restApiClient.CreateSinkConnectorAsync(connectorConfig, SinkConnectorName);
+
+      //Assert
+      httpResponseMessage.IsSuccessStatusCode.Should().BeTrue();
+
+      //Cleanup
+      httpResponseMessage = await restApiClient.DropConnectorAsync($"`{SinkConnectorName}`");
+    }
+
+    private string SourceConnectorName => "mock-source-connector";
+
+    [TestMethod]
+    public async Task CreateSourceConnectorAsync_And_GetConnectorAsync_And_DropConnectorIfExistsAsync()
+    {
+      //Arrange
+      var connectorConfig = new Dictionary<string, string> {
+        { "connector.class", "org.apache.kafka.connect.tools.MockSourceConnector" },
+      };
+
+      //Act
+      var httpResponseMessage = await restApiClient.CreateSourceConnectorAsync(connectorConfig, SourceConnectorName);
 
       var connectorsResponse = await restApiClient.GetConnectorsAsync();
 
       //Assert
       connectorsResponse[0].Connectors.Should().NotBeNull();
-      connectorsResponse[0].Connectors.Any(c => c.Name == ConnectorName).Should().BeTrue();
+      connectorsResponse[0].Connectors.Any(c => c.Name == SourceConnectorName).Should().BeTrue();
       connectorsResponse[0].Type.Should().Be("connector_list");
 
-      httpResponseMessage = await restApiClient.DropConnectorAsync($"`{ConnectorName}`");
+      httpResponseMessage = await restApiClient.DropConnectorAsync($"`{SourceConnectorName}`");
 
       httpResponseMessage = await restApiClient.DropConnectorAsync("UnknownConnector");
       var content1 = await httpResponseMessage.ToStatementResponsesAsync();
@@ -163,8 +186,10 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       var content2 = await httpResponseMessage.ToStatementResponsesAsync();
 
       connectorsResponse = await restApiClient.GetConnectorsAsync();
-      connectorsResponse[0].Connectors.Any(c => c.Name == ConnectorName).Should().BeFalse();
+      connectorsResponse[0].Connectors.Any(c => c.Name == SourceConnectorName).Should().BeFalse();
     }
+
+    #endregion
 
     [TestMethod]
     public async Task GetStreamsAsync()
@@ -175,7 +200,8 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       var streamResponses = await restApiClient.GetStreamsAsync();
 
       //Assert
-      streamResponses[0].StatementText.Should().Be(StatementTemplates.ShowStreams); //TODO: create test stream
+      streamResponses[0].StatementText.Should().Be(StatementTemplates.ShowStreams);
+      streamResponses[0].Streams.Select(c => c.Name).Contains(nameof(MyMoviesStreamTest).ToUpper() + "S").Should().BeTrue();
     }
 
     [TestMethod]
@@ -187,9 +213,9 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       var tablesResponses = await restApiClient.GetTablesAsync();
 
       //Assert
+      tablesResponses[0].StatementText.Should().Be(StatementTemplates.ShowTables);
 
-      Console.WriteLine(string.Join(',', tablesResponses[0].Tables.Select(c => c.Name)));
-      tablesResponses[0].StatementText.Should().Be(StatementTemplates.ShowTables); //TODO: create test
+      tablesResponses[0].Tables.Select(c => c.Name).Contains(nameof(MyMoviesTable).ToUpper() + "S").Should().BeTrue();
     }
 
     [TestMethod]
@@ -201,8 +227,52 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       var topicsResponses = await restApiClient.GetAllTopicsAsync();
 
       //Assert
-      Console.WriteLine(string.Join(',', topicsResponses[0].Topics.Select(c => c.Name)));
-      topicsResponses[0].StatementText.Should().Be(StatementTemplates.ShowAllTopics); //TODO: create test
+      topicsResponses[0].StatementText.Should().Be(StatementTemplates.ShowAllTopics);
+      topicsResponses[0].Topics.Select(c => c.Name).Contains(nameof(MyMoviesTable)).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public async Task TerminatePushQueryAsync()
+    {
+      //Arrange
+      string topicName = "testTableAsSelect";
+
+      var contextOptions = new KSqlDBContextOptions(@"http:\\localhost:8088")
+      {
+        ShouldPluralizeFromItemName = false
+      };
+      await using var context = new KSqlDBContext(contextOptions);
+
+      string createTableAsSelectStatement = context.CreateOrReplaceTableStatement("TestTableAsSelect")
+        .With(new CreationMetadata
+        {
+          KafkaTopic = topicName,
+          KeyFormat = SerializationFormats.Json,
+          ValueFormat = SerializationFormats.Json
+        })
+        .As<object>("TestTable")
+        .ToStatementString();
+
+      var statement = new KSqlDbStatement(createTableAsSelectStatement);
+
+      var response = await restApiClient.ExecuteStatementAsync(statement);
+      var statementResponse = await response.ToStatementResponsesAsync();
+
+      var queries = await restApiClient.GetQueriesAsync();
+
+      var query = queries.SelectMany(c => c.Queries).FirstOrDefault(c => c.SinkKafkaTopics.Contains(topicName));
+
+      //Act
+      statementResponse = await restApiClient.TerminatePersistentQueryAsync(query.Id);
+
+      //Assert
+      query.QueryType.Should().Be("PERSISTENT");
+      query.SinkKafkaTopics.Should().Contain(topicName);
+
+      response.IsSuccessStatusCode.Should().BeTrue();
+
+      statementResponse[0].CommandStatus.Status.Should().Be(SuccessStatus);
+      statementResponse[0].CommandStatus.Message.Should().Be("Query terminated.");
     }
 
     [TestMethod]
@@ -211,12 +281,11 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       //Arrange
 
       //Act
-      var topicsResponses = await restApiClient.GetQueriesAsync();
+      var queriesResponses = await restApiClient.GetQueriesAsync();
 
       //Assert
-
-      Console.WriteLine(string.Join(',', topicsResponses[0].Queries.Select(c => c.Id)));
-      topicsResponses[0].StatementText.Should().Be(StatementTemplates.ShowQueries); //TODO: create test
+      Console.WriteLine(string.Join(',', queriesResponses[0].Queries.Select(c => c.Id)));
+      queriesResponses[0].StatementText.Should().Be(StatementTemplates.ShowQueries);
     }
 
     [TestMethod]
@@ -227,11 +296,11 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       //Act
       var httpResponseMessage = await restApiClient.DropConnectorIfExistsAsync("UnknownConnector");
       var content = await httpResponseMessage.ToStatementResponsesAsync();
-      
+
       //Assert
       content[0].Type.Should().Be("warning_entity");
     }
-    
+
     [TestMethod]
     public async Task DropConnectorAsync_DoesNotExist_ErrorResponse()
     {
@@ -240,7 +309,7 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi
       //Act
       var httpResponseMessage = await restApiClient.DropConnectorAsync("UnknownConnector");
       var content = await httpResponseMessage.ToStatementResponsesAsync();
-      
+
       //Assert
       content[0].Type.Should().Be("error_entity");
     }
