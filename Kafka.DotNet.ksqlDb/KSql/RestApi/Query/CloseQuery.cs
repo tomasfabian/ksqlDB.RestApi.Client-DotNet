12621e03fa751f8bdc28105f1f0b0a352985d76c
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-08-06 22:49:54 +0200

    [Kafka.DotNet.ksqlDB] - CloseQuery model

---
+++ Kafka.DotNet.ksqlDb/KSql/RestApi/Query/CloseQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Kafka.DotNet.ksqlDB.KSql.RestApi.Query
+{
+  public record CloseQuery
+  {
+    [JsonPropertyName("queryId")]
+    public string QueryId { get; set; }
+  }
+}
