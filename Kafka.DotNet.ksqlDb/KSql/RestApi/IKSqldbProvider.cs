48d4e892aa4a5daec3f0c59d754ff4603b755cf5
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-08-26 12:04:46 +0200

    [Kafka.DotNet.ksqlDb] - IKSqlDbProvider RunAsync returns query id

--- Kafka.DotNet.ksqlDb/KSql/RestApi/IKSqldbProvider.cs
+++ Kafka.DotNet.ksqlDb/KSql/RestApi/IKSqldbProvider.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
+using Kafka.DotNet.ksqlDB.KSql.RestApi.Query;
 
 namespace Kafka.DotNet.ksqlDB.KSql.RestApi
 {
   public interface IKSqlDbProvider
   {
     IAsyncEnumerable<T> Run<T>(object parameters, CancellationToken cancellationToken = default);
+    Task<Query<T>> RunAsync<T>(object parameters, CancellationToken cancellationToken = default);
   }
 }
