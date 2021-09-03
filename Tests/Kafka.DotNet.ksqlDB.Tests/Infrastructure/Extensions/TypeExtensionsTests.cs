b624f469727884694e68f61e0342b3466a483f04
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-09-03 21:28:32 +0200

    [Kafka.DotNet.ksqlDb] - TypeExtensions tests

---
+++ Tests/Kafka.DotNet.ksqlDB.Tests/Infrastructure/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Kafka.DotNet.ksqlDB.Infrastructure.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kafka.DotNet.ksqlDB.Tests.Infrastructure.Extensions
+{
+  [TestClass]
+  public class TypeExtensionsTests
+  {
+    [TestMethod]
+    public void IsAnonymousType()
+    {
+      //Arrange
+
+      //Act
+
+      //Assert
+    }
+
+    [TestMethod]
+    public void IsStruct()
+    {
+      //Arrange
+      var type = typeof(ValueTask);
+
+      //Act
+      var typeDefinition = type.IsStruct();
+
+      //Assert
+      typeDefinition.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void IsStruct_PrimitiveType_ReturnsFalse()
+    {
+      //Arrange
+      var type = typeof(int);
+
+      //Act
+      var typeDefinition = type.IsStruct();
+
+      //Assert
+      typeDefinition.Should().BeFalse();
+    }
+
+    enum TestTypes{}
+
+    [TestMethod]
+    public void IsStruct_EnumType_ReturnsFalse()
+    {
+      //Arrange
+      var type = typeof(TestTypes);
+
+      //Act
+      var typeDefinition = type.IsStruct();
+
+      //Assert
+      typeDefinition.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void IsDictionary()
+    {
+      //Arrange
+      var type = typeof(IDictionary<string, int>);
+
+      //Act
+      var typeDefinition = type.IsDictionary();
+
+      //Assert
+      typeDefinition.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void GetEnumerableTypeDefinition_FindsEnumerableType()
+    {
+      //Arrange
+      var type = typeof(TestEnumerable);
+
+      //Act
+      var typeDefinition = type.GetEnumerableTypeDefinition();
+
+      //Assert
+      typeDefinition.Should().Contain(typeof(IEnumerable<string>));
+    }
+
+    [TestMethod]
+    public void GetEnumerableTypeDefinition_ReturnsNullForNonEnumerableTypes()
+    {
+      //Arrange
+      var type = typeof(int);
+
+      //Act
+      var typeDefinition = type.GetEnumerableTypeDefinition();
+
+      //Assert
+      typeDefinition.Should().BeEmpty();
+    }
+
+    class TestEnumerable : List<string>
+    {
+
+    }
+  }
+}
