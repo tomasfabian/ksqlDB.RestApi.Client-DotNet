354d96f8891f7a45f9db723adb1f102441ddc166
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-09-07 20:11:49 +0200

    [Kafka.DotNet.ksqlDb] - EnumerableExtensionsTests

---
+++ Tests/Kafka.DotNet.ksqlDB.Tests/Infrastructure/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Kafka.DotNet.ksqlDB.Infrastructure.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kafka.DotNet.ksqlDB.Tests.Infrastructure.Extensions
+{
+  [TestClass]
+  public class EnumerableExtensionsTests
+  {
+    [TestMethod]
+    public void IsOneOfFollowing()
+    {
+      //Arrange
+
+      //Act
+      var isOneOf = 1.IsOneOfFollowing(1, 2, 3);
+
+      //Assert
+      isOneOf.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void IsOneOfFollowing_ReturnsFalse()
+    {
+      //Arrange
+
+      //Act
+      var isOneOf = 4.IsOneOfFollowing(1, 2, 3);
+
+      //Assert
+      isOneOf.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void IsNotOneOfFollowing()
+    {
+      //Arrange
+
+      //Act
+      var isNotOneOf = 4.IsNotOneOfFollowing(1, 2, 3);
+
+      //Assert
+      isNotOneOf.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void IsNotOneOfFollowing_ReturnsFalse()
+    {
+      //Arrange
+
+      //Act
+      var isNotOneOf = 1.IsNotOneOfFollowing(1, 2, 3);
+
+      //Assert
+      isNotOneOf.Should().BeFalse();
+    }
+  }
+}
