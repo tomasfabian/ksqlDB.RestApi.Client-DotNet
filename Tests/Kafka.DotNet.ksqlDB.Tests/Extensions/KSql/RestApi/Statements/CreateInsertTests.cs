46905ed06713ccaea9e034d9a4b40165e760c0b8
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-09-09 07:45:04 +0200

    [Kafka.DotNet.ksqlDb] - generate insert tests

--- Tests/Kafka.DotNet.ksqlDB.Tests/Extensions/KSql/RestApi/Statements/CreateInsertTests.cs
+++ Tests/Kafka.DotNet.ksqlDB.Tests/Extensions/KSql/RestApi/Statements/CreateInsertTests.cs
@@ -1,5 +1,7 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using Kafka.DotNet.ksqlDB.KSql.RestApi.Statements;
+using Kafka.DotNet.ksqlDB.KSql.RestApi.Statements.Annotations;
 using Kafka.DotNet.ksqlDB.KSql.RestApi.Statements.Properties;
 using Kafka.DotNet.ksqlDB.Tests.Models.Movies;
 using NUnit.Framework;
@@ -71,6 +73,124 @@ namespace Kafka.DotNet.ksqlDB.Tests.Extensions.KSql.RestApi.Statements
 
       //Assert
       statement.Should().Be(@$"INSERT INTO {nameof(Movie)} (Title, Id, Release_Year) VALUES ('Title', 1, 1988);");
+    }
+
+    record EventCategory
+    {
+      public int Count { get; set; }
+      public string Name { get; set; }
+    }
+
+    record Event
+    {
+      [Key]
+      public int Id { get; set; }
+
+      public string[] Places { get; set; }
+    }
+
+    [Test]
+    public void StringEnumerableMemberType()
+    {
+      //Arrange
+      var testEvent = new Event
+      {
+        Id = 1,
+        Places = new[] { "Place1", "Place2", "Place3" },
+      };
+
+      //Act
+      string statement = new CreateInsert().Generate(testEvent);
+
+      //Assert
+      statement.Should().Be(@"INSERT INTO Events (Id, Places) VALUES (1, ARRAY['Place1','Place2','Place3']);");
+    }
+    
+    record EventWithPrimitiveEnumerable
+    {
+      [Key]
+      public string Id { get; set; }
+
+      public int[] Places { get; set; }
+    }
+
+    [Test]
+    public void PrimitiveEnumerableMemberType()
+    {
+      //Arrange
+      var testEvent = new EventWithPrimitiveEnumerable
+      {
+        Id = "1",
+        Places = new[] { 1, 2, 3 }
+      };
+
+      //Act
+      string statement = new CreateInsert().Generate(testEvent, new InsertProperties { EntityName = "Events"});
+
+      //Assert
+      statement.Should().Be(@"INSERT INTO Events (Id, Places) VALUES ('1', ARRAY[1,2,3]);");
+    }
+    
+    record EventWithList
+    {
+      [Key]
+      public string Id { get; set; }
+
+      public List<int> Places { get; set; }
+    }
+
+    [Test]
+    public void PrimitiveListMemberType()
+    {
+      //Arrange
+      var testEvent = new EventWithList
+      {
+        Id = "1",
+        Places = new List<int> { 1, 2, 3 }
+      };
+
+      //Act
+      string statement = new CreateInsert().Generate(testEvent, new InsertProperties { EntityName = "Events"});
+
+      //Assert
+      statement.Should().Be(@"INSERT INTO Events (Id, Places) VALUES ('1', ARRAY[1,2,3]);");
+    }
+
+    [Test]
+    public void ComplexListMemberType()
+    {
+    }
+
+    struct ComplexEvent
+    {
+      [Key]
+      public int Id { get; set; }
+
+      public EventCategory Category { get; set; }
+    }
+
+    [Test]
+    public void ComplexType()
+    {
+      //Arrange
+      var eventCategory = new EventCategory
+      {
+        Count = 1,
+        Name = "Planet Earth"
+      };
+
+      var testEvent = new ComplexEvent
+      {
+        Id = 1,
+        Category = eventCategory
+        //Categories = new[] { eventCategory, new EventCategory { Name = "Discovery" } }
+      };
+
+      //Act
+      string statement = new CreateInsert().Generate(testEvent, new InsertProperties { EntityName = "Events"});
+
+      //Assert
+      statement.Should().Be(@"INSERT INTO Events (Id, Category) VALUES (1, STRUCT(Count := 1, Name := 'Planet Earth'));");
     }
   }
 }
