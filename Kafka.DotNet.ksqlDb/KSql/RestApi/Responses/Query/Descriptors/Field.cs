4e659bcc63271cc4c606fd952c815a16c5fa610e
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-09-15 20:17:21 +0200

    [Kafka.DotNet.ksqlDB] - Explain response split classes to files

---
+++ Kafka.DotNet.ksqlDb/KSql/RestApi/Responses/Query/Descriptors/Field.cs
@@ -0,0 +1,9 @@
+﻿namespace Kafka.DotNet.ksqlDB.KSql.RestApi.Responses.Query.Descriptors
+{
+  public record Field
+  {
+    public string Name { get; set; }
+    public Schema Schema { get; set; }
+    public string Type { get; set; }
+  }
+}
