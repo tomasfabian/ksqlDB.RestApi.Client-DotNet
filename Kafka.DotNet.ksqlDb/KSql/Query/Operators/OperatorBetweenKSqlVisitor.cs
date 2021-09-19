57831cf4af4dadc7e80095f651a2c3b41b646198
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-09-19 13:43:04 +0200

    [Kafka.DotNet.ksqlDB] - OperatorBetweenKSqlVisitor

---
+++ Kafka.DotNet.ksqlDb/KSql/Query/Operators/OperatorBetweenKSqlVisitor.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace Kafka.DotNet.ksqlDB.KSql.Query.Operators
+{
+  internal class OperatorBetweenKSqlVisitor : KSqlVisitor
+  {
+    public OperatorBetweenKSqlVisitor(StringBuilder stringBuilder)
+      : base(stringBuilder, useTableAlias: false)
+    {
+    }
+
+    protected override Expression VisitMethodCall(MethodCallExpression methodCallExpression)
+    {
+      var methodInfo = methodCallExpression.Method;
+
+      if (methodCallExpression.Object == null
+          && methodInfo.DeclaringType.Name == nameof(KSqlOperatorExtensions))
+      {
+        switch (methodInfo.Name)
+        {
+          case nameof(KSqlOperatorExtensions.Between):
+            
+            Visit(methodCallExpression.Arguments[0]);
+
+            Append(" BETWEEN ");
+
+            Visit(methodCallExpression.Arguments[1]);
+
+            Append(" AND ");
+
+            Visit(methodCallExpression.Arguments[2]);
+            
+            break;
+        }
+
+      }
+      else base.VisitMethodCall(methodCallExpression);
+
+      return methodCallExpression;
+    }
+  }
+}
