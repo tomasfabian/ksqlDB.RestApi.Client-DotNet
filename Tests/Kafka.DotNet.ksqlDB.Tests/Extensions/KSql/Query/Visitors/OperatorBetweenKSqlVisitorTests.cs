8a6effbc7d78214e7ae4bb50bddea0fa1be51856
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-09-20 07:52:18 +0200

    [Kafka.DotNet.ksqlDB] - OperatorBetweenKSqlVisitor Tests

---
+++ Tests/Kafka.DotNet.ksqlDB.Tests/Extensions/KSql/Query/Visitors/OperatorBetweenKSqlVisitorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+using FluentAssertions;
+using Kafka.DotNet.ksqlDB.KSql.Query.Operators;
+using Kafka.DotNet.ksqlDB.Tests.Pocos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests;
+
+namespace Kafka.DotNet.ksqlDB.Tests.Extensions.KSql.Query.Visitors
+{
+  [TestClass]
+  public class OperatorBetweenKSqlVisitorTests : TestBase
+  {
+    [TestMethod]
+    public void Visit()
+    {
+      //Arrange
+      Expression<Func<Tweet, bool>> expression = t => t.Id.Between(1, 100);
+      StringBuilder stringBuilder = new();
+
+      //Act
+      new OperatorBetweenKSqlVisitor(stringBuilder).Visit(expression);
+      var ksql = stringBuilder.ToString();
+
+      //Assert
+      ksql.Should().Be("Id BETWEEN 1 AND 100");
+    }
+  }
+}
