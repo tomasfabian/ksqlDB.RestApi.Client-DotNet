e09bf8aa90112cc841876ddfb9562fc5a70cee45
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-09-24 07:02:13 +0200

    [Kafka.DotNet.ksqlDB] - KSqlInvocationFunctionsExtensions

---
+++ Kafka.DotNet.ksqlDb/KSql/Query/Functions/KSqlInvocationFunctionsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kafka.DotNet.ksqlDB.KSql.Query.Functions
+{
+  internal static class KSqlInvocationFunctionsExtensions
+  {
+    /// <summary>
+    /// Transform a collection by using a lambda function.
+    /// </summary>
+    public static U Transform<T, U>(this KSqlFunctions kSqlFunctions, T[] array, Func<T, U> selector)
+    {
+      throw new InvalidOperationException(KSqlFunctionsExtensions.ServerSideOperationErrorMessage);
+    }
+  }
+}
