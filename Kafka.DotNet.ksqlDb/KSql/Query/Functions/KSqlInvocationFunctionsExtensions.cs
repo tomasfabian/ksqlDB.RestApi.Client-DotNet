bcdc5081e378728ac0e0d18c11518d0ffaa6ad04
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-09-27 07:36:15 +0200

    [Kafka.DotNet.ksqlDB] - Maps Transform, Reduce and Filters invocation functions

--- Kafka.DotNet.ksqlDb/KSql/Query/Functions/KSqlInvocationFunctionsExtensions.cs
+++ Kafka.DotNet.ksqlDb/KSql/Query/Functions/KSqlInvocationFunctionsExtensions.cs
@@ -35,6 +35,26 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Functions
     }
 
     /// <summary>
+    /// When transforming a map, two functions must be provided. For each map entry, the first function provided
+    /// will be applied to the key and the second one applied to the value. Each function must have two
+    /// arguments. The two arguments for each function are in order: the key and then the value. The transformed
+    /// map is returned.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TKeyResult"></typeparam>
+    /// <typeparam name="TValueResult"></typeparam>
+    /// <param name="kSqlFunctions"></param>
+    /// <param name="source"></param>
+    /// <param name="keySelector"></param>
+    /// <param name="valueSelector"></param>
+    /// <returns>Apply a function to each element in a collection. The transformed collection is returned.</returns>
+    internal static IDictionary<TKeyResult, TValueResult> Transform<TKey, TValue, TKeyResult, TValueResult>(this KSqlFunctions kSqlFunctions, IDictionary<TKey, TValue> source, Func<TKey, TValue, TKeyResult> keySelector, Func<TKey, TValue, TValueResult> valueSelector)
+    {
+      throw new InvalidOperationException(KSqlFunctionsExtensions.ServerSideOperationErrorMessage);
+    }
+
+    /// <summary>
     /// Filter a collection with a lambda function.
     /// </summary>
     /// <typeparam name="TSource">The type of the elements of <paramref name="array" />.</typeparam>
@@ -50,7 +70,7 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Functions
     /// <summary>
     /// Filter a collection with a lambda function.
     /// </summary>
-    /// <typeparam name="TSource">The type of the elements of <paramref name="array" />.</typeparam>
+    /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
     /// <param name="kSqlFunctions"></param>
     /// <param name="source">The enumerable source.</param>
     /// <param name="predicate">A function to test each element for a condition.</param>
@@ -58,8 +78,23 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Functions
     public static TSource[] Filter<TSource>(this KSqlFunctions kSqlFunctions, IEnumerable<TSource> source, Func<TSource, bool> predicate)
     {
       throw new InvalidOperationException(KSqlFunctionsExtensions.ServerSideOperationErrorMessage);
-    }    
-    
+    }
+
+    /// <summary>
+    /// When filtering a map, the function provided must have a boolean result. For each map entry, the function
+    /// will be applied to the key and value arguments in that order. The filtered map is returned.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="kSqlFunctions"></param>
+    /// <param name="dictionary">The dictionary.</param>
+    /// <param name="predicate">A function to test each element for a condition.</param>
+    /// <returns>Filter the input collection through a given lambda function. The filtered collection is returned.</returns>
+    internal static IDictionary<TKey, TValue> Filter<TKey, TValue>(this KSqlFunctions kSqlFunctions, IDictionary<TKey, TValue> dictionary, Func<TKey, TValue, bool> predicate)
+    {
+      throw new InvalidOperationException(KSqlFunctionsExtensions.ServerSideOperationErrorMessage);
+    } 
+
     /// <summary>
     /// Reduce a collection starting from an initial state.
     /// </summary>
@@ -68,7 +103,7 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Functions
     /// <param name="state">The initial state.</param>
     /// <param name="accumulator">The reduce function.</param>
     /// <returns>The accumulated state.</returns>
-    public static TResult Reduce<TSource, TResult>(this KSqlFunctions kSqlFunctions, TSource[] array, TResult state, Func<TSource, TResult, TResult> accumulator)
+    public static TState Reduce<TSource, TState>(this KSqlFunctions kSqlFunctions, TSource[] array, TState state, Func<TState, TSource, TState> accumulator)
     {
       throw new InvalidOperationException(KSqlFunctionsExtensions.ServerSideOperationErrorMessage);
     }
@@ -81,7 +116,25 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Functions
     /// <param name="state">The initial state.</param>
     /// <param name="accumulator">The reduce function.</param>
     /// <returns>The accumulated state.</returns>
-    public static TResult Reduce<TSource, TResult>(this KSqlFunctions kSqlFunctions, IEnumerable<TSource> source, TResult state, Func<TSource, TResult, TResult> accumulator)
+    public static TState Reduce<TSource, TState>(this KSqlFunctions kSqlFunctions, IEnumerable<TSource> source, TState state, Func<TState, TSource, TState> accumulator)
+    {
+      throw new InvalidOperationException(KSqlFunctionsExtensions.ServerSideOperationErrorMessage);
+    }
+
+    /// <summary>
+    /// Reduce the input collection down to a single value using an initial state and a function. The initial
+    /// state (s) is passed into the scope of the function. Each invocation returns a new value for s, which the
+    /// next invocation will receive. The final value for s is returned.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TState"></typeparam>
+    /// <param name="kSqlFunctions"></param>
+    /// <param name="dictionary">The dictionary</param>
+    /// <param name="state">The initial state.</param>
+    /// <param name="accumulator"></param>
+    /// <returns>When reducing a map, the reduce function must have three arguments. The three arguments for the reduce function are in order: the state, the key, and the value. The final state is returned.</returns>
+    internal static TState Reduce<TKey, TSource, TState>(this KSqlFunctions kSqlFunctions, IDictionary<TKey, TSource> dictionary, TState state, Func<TState, TKey, TSource, TState> accumulator)
     {
       throw new InvalidOperationException(KSqlFunctionsExtensions.ServerSideOperationErrorMessage);
     }
