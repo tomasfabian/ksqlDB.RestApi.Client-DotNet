12a22997fcd3203becb47f119705dd2eaebcf897
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-11-05 21:45:59 +0100

    [ksqlDB.RestApi.Client] - LoggerMock Extensions

---
+++ Tests/ksqlDB.RestApi.Client.Tests/Fakes/Logging/LoggerMockExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ksqlDB.Api.Client.Tests.Fakes.Logging
+{
+  internal static class LoggerMockExtensions
+  {
+    public static void VerifyLog(this Mock<ILogger> mockedLogger, LogLevel logLevel, Func<Times> times)
+    {
+      mockedLogger.Verify(c => c.Log(
+                            It.Is<LogLevel>(l => l == logLevel),
+                            It.IsAny<EventId>(),
+                            It.Is<It.IsAnyType>((v, t) => true),
+                            It.IsAny<Exception>(),
+                            It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), times);
+    }
+  }
+}
