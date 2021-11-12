8c5c2c1983b968e6d215b7d061b0541d8de6805a
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-11-12 07:58:04 +0100

    [ksqlDB.RestApi.Client] - KSqlDBContext SaveChangesAsync integration test

---
+++ Tests/ksqlDB.RestApi.Client.IntegrationTests/KSql/Query/Context/KSqlDbContextTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ksqlDB.Api.Client.IntegrationTests.Models.Movies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ksqlDB.Api.Client.IntegrationTests.KSql.Query.Context
+{
+  [TestClass]
+  public class KSqlDbContextTests : Infrastructure.IntegrationTests
+  {
+    [TestMethod]
+    public async Task AddAndSaveChangesAsync()
+    {
+      //Arrange
+      var entity1 = new Movie { Id = 1, Title = "T1" };
+      var entity2 = new Movie { Id = 2, Title = "T2" };
+
+      //Act
+      Context.Add(entity1);
+      Context.Add(entity2);
+
+      var response = await Context.SaveChangesAsync();
+
+      //Assert
+      response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+  }
+}
