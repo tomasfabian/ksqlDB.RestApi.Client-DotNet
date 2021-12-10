92186c32aa19a2459be1c12f42e6fe81c744ae3b
Tomas Fabian <t.fabian@cid.com>
2021-12-10 22:14:51 +0100

    [ksqlDB.RestApi.Client] - TimeSpanToStringConverter

---
+++ ksqlDb.RestApi.Client/KSql/Query/Context/JsonConverters/TimeSpanToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ksqlDB.RestApi.Client.KSql.Query.Context.JsonConverters
+{
+  public class TimeSpanToStringConverter : JsonConverter<TimeSpan>
+  {
+    public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+      var value = reader.GetString();
+
+      return TimeSpan.Parse(value);
+    }
+
+    public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+    {
+      string convertedValue = value.ToString();
+
+      writer.WriteStringValue(convertedValue);
+    }
+  }
+}
--- ksqlDb.RestApi.Client/KSql/Query/Context/Options/KSqlDbJsonSerializerOptions.cs
+++ ksqlDb.RestApi.Client/KSql/Query/Context/Options/KSqlDbJsonSerializerOptions.cs
@@ -1,12 +1,20 @@
 ﻿using System.Text.Json;
+using ksqlDB.RestApi.Client.KSql.Query.Context.JsonConverters;
 
 namespace ksqlDb.RestApi.Client.KSql.Query.Context.Options
 {
   internal static class KSqlDbJsonSerializerOptions
   {
-    public static JsonSerializerOptions CreateInstance() => new()
-                                                            {
-                                                              PropertyNameCaseInsensitive = true
-                                                            };
+    public static JsonSerializerOptions CreateInstance()
+    {
+      JsonSerializerOptions jsonSerializerOptions = new()
+      {
+        PropertyNameCaseInsensitive = true
+      };
+
+      jsonSerializerOptions.Converters.Add(new TimeSpanToStringConverter());
+
+      return jsonSerializerOptions;
+    }
   }
 }
