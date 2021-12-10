f33b14e90e84b7d84b36f8e4770fc9e551740d23
Tomas Fabian <t.fabian@cid.com>
2021-12-10 22:26:57 +0100

    [ksqlDB.RestApi.Client] - KSqlDbJsonSerializerOptions Tests

---
+++ Tests/ksqlDB.RestApi.Client.Tests/KSql/Query/Context/Options/KSqlDbJsonSerializerOptionsTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using FluentAssertions;
+using ksqlDB.RestApi.Client.KSql.Query.Context.JsonConverters;
+using ksqlDb.RestApi.Client.KSql.Query.Context.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ksqlDB.Api.Client.Tests.KSql.Query.Context.Options
+{
+  [TestClass]
+  public class KSqlDbJsonSerializerOptionsTests
+  {
+    [TestMethod]
+    public void CreateInstance_PropertyNameCaseInsensitive()
+    {
+      //Arrange
+      var options = KSqlDbJsonSerializerOptions.CreateInstance();
+
+      //Act
+      var propertyNameCaseInsensitive = options.PropertyNameCaseInsensitive;
+
+      //Assert
+      propertyNameCaseInsensitive.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void CreateInstance_Converters()
+    {
+      //Arrange
+      var options = KSqlDbJsonSerializerOptions.CreateInstance();
+
+      //Act
+      var converters = options.Converters;
+
+      //Assert
+      converters.OfType<TimeSpanToStringConverter>().Any().Should().BeTrue();
+    }
+  }
+}
