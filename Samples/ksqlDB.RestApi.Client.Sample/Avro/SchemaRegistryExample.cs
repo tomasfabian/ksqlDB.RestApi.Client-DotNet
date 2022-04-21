f056c8a1b3c88595f1498b303e42765487ad38f3
Tomas Fabian <t.fabian@cid.com>
2022-04-21 07:25:51 +0200

    [ksqlDB.RestApi.Client] - create stream values with Avro format example

---
+++ Samples/ksqlDB.RestApi.Client.Sample/Avro/SchemaRegistryExample.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.SchemaRegistry;
+using Sensors;
+using ksqlDB.Api.Client.Samples.Providers;
+using ksqlDB.RestApi.Client.KSql.RestApi.Http;
+using ksqlDB.RestApi.Client.KSql.RestApi.Serialization;
+using ksqlDB.RestApi.Client.KSql.RestApi.Statements;
+
+namespace ksqlDB.Api.Client.Samples.Avro
+{
+  public class SchemaRegistryExample
+  {
+    public async Task AvroExampleAsync(string ksqlDbUrl)
+    {
+      var schemaRegistryConfig = new SchemaRegistryConfig
+      {
+        Url = "http://localhost:8081"
+      };
+      
+      using var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+
+      var schema = IoTSensor._SCHEMA.ToString();
+
+      var subject = "IoTSensors-value";
+
+      var registrationResult = await schemaRegistry.RegisterSchemaAsync(subject, schema);
+
+      //http://localhost:8081/subjects/IoTSensors-value/versions/latest/schema
+      var latestSchema = await schemaRegistry.GetLatestSchemaAsync(subject);
+
+      var httpClientFactory = new HttpClientFactory(new Uri(ksqlDbUrl));
+      var restApiClient = new KSqlDbRestApiProvider(httpClientFactory);
+
+      EntityCreationMetadata metadata = new()
+      {
+        EntityName = "avroSensors",
+        KafkaTopic = "IoTSensors",
+        ValueFormat = SerializationFormats.Avro,
+        Partitions = 1,
+        Replicas = 1
+      };
+      
+      var httpResponseMessage = await restApiClient.CreateStreamAsync<IoTSensor>(metadata, ifNotExists: false)
+        .ConfigureAwait(false);
+
+      var httpResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+
+      Console.WriteLine(httpResponse);
+
+      // Stream Name         | Kafka Topic | Key Format | Value Format | Windowed
+      //   ------------------------------------------------------------------------------------------------------
+      // AVROSENSORS | IoTSensors | KAFKA | AVRO | false
+    }
+  }
+}
