97377960e367153b1d18695bdbc0598639bbfa91
Tomas Fabian <t.fabian@cid.com>
2022-07-16 11:57:53 +0200

    [ksqlDB.RestApi.Client]: UnitTests file scoped namespaces

--- Tests/UnitTests/Schedulers/ReactiveTestSchedulersFactory.cs
+++ Tests/UnitTests/Schedulers/ReactiveTestSchedulersFactory.cs
@@ -2,50 +2,49 @@
 using Joker.Factories.Schedulers;
 using Microsoft.Reactive.Testing;
 
-namespace UnitTests.Schedulers
+namespace UnitTests.Schedulers;
+
+public class ReactiveTestSchedulersFactory : ISchedulersFactory
 {
-  public class ReactiveTestSchedulersFactory : ISchedulersFactory
-  {
-    private TestScheduler currentThread;
+  private TestScheduler currentThread;
 
-    public TestScheduler CurrentThread => currentThread ?? (currentThread = new TestScheduler());
+  public TestScheduler CurrentThread => currentThread ?? (currentThread = new TestScheduler());
 
-    private TestScheduler immediate;
+  private TestScheduler immediate;
 
-    public TestScheduler Immediate => immediate ?? (immediate = new TestScheduler());
+  public TestScheduler Immediate => immediate ?? (immediate = new TestScheduler());
 
-    private TestScheduler newThread;
+  private TestScheduler newThread;
 
-    public TestScheduler NewThread => newThread ?? (newThread = new TestScheduler());
+  public TestScheduler NewThread => newThread ?? (newThread = new TestScheduler());
 
-    private TestScheduler taskPool;
+  private TestScheduler taskPool;
 
-    public TestScheduler TaskPool => taskPool ?? (taskPool = new TestScheduler());
+  public TestScheduler TaskPool => taskPool ?? (taskPool = new TestScheduler());
 
-    private TestScheduler threadPool;
+  private TestScheduler threadPool;
 
-    public TestScheduler ThreadPool => threadPool ?? (threadPool = new TestScheduler());
+  public TestScheduler ThreadPool => threadPool ?? (threadPool = new TestScheduler());
 
-    public IScheduler EventLoopScheduler => threadPool ?? (threadPool = new TestScheduler());
+  public IScheduler EventLoopScheduler => threadPool ?? (threadPool = new TestScheduler());
 
-    public IScheduler NewEventLoopScheduler => threadPool ?? (threadPool = new TestScheduler());
+  public IScheduler NewEventLoopScheduler => threadPool ?? (threadPool = new TestScheduler());
 
-    private TestScheduler dispatcher;
+  private TestScheduler dispatcher;
 
-    public TestScheduler Dispatcher => dispatcher ?? (dispatcher = new TestScheduler());
+  public TestScheduler Dispatcher => dispatcher ?? (dispatcher = new TestScheduler());
 
-    #region ISchedulerProvider Members
+  #region ISchedulerProvider Members
 
-    IScheduler ISchedulersFactory.CurrentThread => CurrentThread;
+  IScheduler ISchedulersFactory.CurrentThread => CurrentThread;
 
-    IScheduler ISchedulersFactory.Immediate => Immediate;
+  IScheduler ISchedulersFactory.Immediate => Immediate;
 
-    IScheduler ISchedulersFactory.NewThread => NewThread;
+  IScheduler ISchedulersFactory.NewThread => NewThread;
 
-    IScheduler ISchedulersFactory.TaskPool => TaskPool;
+  IScheduler ISchedulersFactory.TaskPool => TaskPool;
 
-    IScheduler ISchedulersFactory.ThreadPool => ThreadPool;
+  IScheduler ISchedulersFactory.ThreadPool => ThreadPool;
 
-    #endregion
-  }
+  #endregion
 }
--- Tests/UnitTests/TestBase.cs
+++ Tests/UnitTests/TestBase.cs
@@ -5,73 +5,72 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ninject.MockingKernel.Moq;
 using UnitTests.Schedulers;
 
-namespace UnitTests
+namespace UnitTests;
+
+public abstract class TestBase<TClassUnderTest> : TestBase
 {
-  public abstract class TestBase<TClassUnderTest> : TestBase
+  protected TClassUnderTest ClassUnderTest { get; set; }
+}
+
+public abstract class TestBase
+{
+  protected readonly MoqMockingKernel MockingKernel = new MoqMockingKernel();
+  protected TestScheduler TestScheduler = new TestScheduler();
+
+  protected ReactiveTestSchedulersFactory SchedulersFactory;
+
+  [TestInitialize]
+  public virtual void TestInitialize()
   {
-    protected TClassUnderTest ClassUnderTest { get; set; }
+    SchedulersFactory = new ReactiveTestSchedulersFactory();
+
+    MockingKernel.Bind<IScheduler>().ToConstant(TestScheduler);
+    MockingKernel.Bind<ReactiveTestSchedulersFactory>().ToConstant(SchedulersFactory);
   }
 
-  public abstract class TestBase
+  [TestCleanup]
+  public virtual void TestCleanup()
   {
-    protected readonly MoqMockingKernel MockingKernel = new MoqMockingKernel();
-    protected TestScheduler TestScheduler = new TestScheduler();
+  }
 
-    protected ReactiveTestSchedulersFactory SchedulersFactory;
+  #region RunSchedulers
 
-    [TestInitialize]
-    public virtual void TestInitialize()
-    {
-      SchedulersFactory = new ReactiveTestSchedulersFactory();
-
-      MockingKernel.Bind<IScheduler>().ToConstant(TestScheduler);
-      MockingKernel.Bind<ReactiveTestSchedulersFactory>().ToConstant(SchedulersFactory);
-    }
-
-    [TestCleanup]
-    public virtual void TestCleanup()
-    {
-    }
-
-    #region RunSchedulers
-
-    public void RunSchedulers()
-    {
-      TestScheduler.Start();
+  public void RunSchedulers()
+  {
+    TestScheduler.Start();
       
-      SchedulersFactory.ThreadPool.Start();
-      SchedulersFactory.TaskPool.Start();
-      SchedulersFactory.Dispatcher.Start();
-    }
+    SchedulersFactory.ThreadPool.Start();
+    SchedulersFactory.TaskPool.Start();
+    SchedulersFactory.Dispatcher.Start();
+  }
 
-    #endregion
+  #endregion
 
-    #region AdvanceSchedulers
+  #region AdvanceSchedulers
 
-    public void AdvanceSchedulers(long time)
-    {
-      TestScheduler.AdvanceBy(time);
+  public void AdvanceSchedulers(long time)
+  {
+    TestScheduler.AdvanceBy(time);
       
-      SchedulersFactory.ThreadPool.AdvanceBy(time);
-      SchedulersFactory.TaskPool.AdvanceBy(time);
-      SchedulersFactory.Dispatcher.AdvanceBy(time);
-    }
+    SchedulersFactory.ThreadPool.AdvanceBy(time);
+    SchedulersFactory.TaskPool.AdvanceBy(time);
+    SchedulersFactory.Dispatcher.AdvanceBy(time);
+  }
 
-    #endregion    
+  #endregion    
     
-    protected void Schedule(TestScheduler testScheduler, TimeSpan timeSpan, Action action)
-    {
-      testScheduler.Schedule(timeSpan, action);
-    }
+  protected void Schedule(TestScheduler testScheduler, TimeSpan timeSpan, Action action)
+  {
+    testScheduler.Schedule(timeSpan, action);
+  }
     
-    protected void ScheduleOnTaskPool(TimeSpan timeSpan, Action action)
-    {
-      Schedule(SchedulersFactory.TaskPool, timeSpan, action);
-    }
+  protected void ScheduleOnTaskPool(TimeSpan timeSpan, Action action)
+  {
+    Schedule(SchedulersFactory.TaskPool, timeSpan, action);
+  }
     
-    protected void ScheduleOnThreadPool(TimeSpan timeSpan, Action action)
-    {
-      Schedule(SchedulersFactory.ThreadPool, timeSpan, action);
-    }
+  protected void ScheduleOnThreadPool(TimeSpan timeSpan, Action action)
+  {
+    Schedule(SchedulersFactory.ThreadPool, timeSpan, action);
   }
 }
