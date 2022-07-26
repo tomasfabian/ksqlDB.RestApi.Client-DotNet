5abd29e921821340f157de83b5461a3001dbbc42
Tomas Fabian <t.fabian@cid.com>
2022-07-26 15:34:00 +0200

    [ksqlDB.RestApi.Client]: AssertSchema

---
+++ ksqlDb.RestApi.Client/KSql/RestApi/Generators/Asserts/AssertSchema.cs
@@ -0,0 +1,19 @@
+﻿namespace ksqlDb.RestApi.Client.KSql.RestApi.Generators.Asserts;
+
+internal static class AssertSchema
+{
+  public static string CreateStatement(bool exists, AssertSchemaOptions options)
+  {
+    var notExists = exists ? string.Empty : "NOT EXISTS ";
+
+    var subject = string.IsNullOrEmpty(options.SubjectName) ? string.Empty : $" SUBJECT '{options.SubjectName}'";
+
+    var idValue = options.Id.HasValue ? $" ID {options.Id.Value}" : string.Empty;
+
+    var timeOut = options.Timeout != null ? $" TIMEOUT {options.Timeout.Value} {options.Timeout.TimeUnit}" : string.Empty;
+
+    var statement = $@"ASSERT {notExists}SCHEMA{subject}{idValue}{timeOut};";
+
+    return statement;
+  }
+}
---
+++ ksqlDb.RestApi.Client/KSql/RestApi/Generators/Asserts/AssertSchemaOptions.cs
@@ -0,0 +1,21 @@
+﻿using ksqlDB.RestApi.Client.KSql.Query.Windows;
+
+namespace ksqlDb.RestApi.Client.KSql.RestApi.Generators.Asserts;
+
+public record AssertSchemaOptions
+{
+  public AssertSchemaOptions(string subjectName, int? id = null)
+  {
+    if (string.IsNullOrEmpty(subjectName) && !id.HasValue)
+      throw new ArgumentException("Assert schema statements must include a subject name or id");
+
+    SubjectName = subjectName;
+    Id = id;
+  }
+
+  public string SubjectName { get; }
+  
+  public int? Id { get; }
+
+  public Duration Timeout { get; set; }
+}
