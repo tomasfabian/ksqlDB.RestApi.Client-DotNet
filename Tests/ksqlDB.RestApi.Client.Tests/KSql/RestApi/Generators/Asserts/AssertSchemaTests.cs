b8171400807b44994d2ba909d4642742a6d3d86f
Tomas Fabian <t.fabian@cid.com>
2022-07-26 16:04:48 +0200

    [ksqlDB.RestApi.Client]: AssertSchema unit tests

---
+++ Tests/ksqlDB.RestApi.Client.Tests/KSql/RestApi/Generators/Asserts/AssertSchemaTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using ksqlDB.RestApi.Client.KSql.Query.Windows;
+using ksqlDb.RestApi.Client.KSql.RestApi.Generators.Asserts;
+using NUnit.Framework;
+
+namespace ksqlDB.Api.Client.Tests.KSql.RestApi.Generators.Asserts;
+
+public class AssertSchemaTests
+{
+  string subjectName = "Kafka-key";
+
+  [Test]
+  public void CreateStatement_SubjectExists()
+  {
+    //Arrange
+    var options = new AssertSchemaOptions(subjectName);
+
+    //Act
+    string statement = AssertSchema.CreateStatement(exists: true, options);
+
+    //Assert
+    statement.Should().Be($@"ASSERT SCHEMA SUBJECT '{subjectName}';");
+  }
+
+  int schemaId = 21;
+
+  [Test]
+  public void CreateStatement_SubjectIdExists()
+  {
+    //Arrange
+    var options = new AssertSchemaOptions(null, schemaId);
+
+    //Act
+    string statement = AssertSchema.CreateStatement(exists: true, options);
+
+    //Assert
+    statement.Should().Be($@"ASSERT SCHEMA ID {schemaId};");
+  }
+
+  [Test]
+  public void CreateStatement_SubjectIdAndNameExists()
+  {
+    //Arrange
+    var options = new AssertSchemaOptions(subjectName, schemaId);
+
+    //Act
+    string statement = AssertSchema.CreateStatement(exists: true, options);
+
+    //Assert
+    statement.Should().Be($@"ASSERT SCHEMA SUBJECT '{subjectName}' ID {schemaId};");
+  }
+
+  [Test]
+  public void CreateStatement_Timeout()
+  {
+    //Arrange
+    var timeout = Duration.OfSeconds(10);
+    var options = new AssertSchemaOptions(subjectName)
+    {
+      Timeout = timeout
+    };
+
+    //Act
+    string statement = AssertSchema.CreateStatement(exists: true, options);
+
+    //Assert
+    statement.Should().Be($@"ASSERT SCHEMA SUBJECT '{subjectName}' TIMEOUT {timeout.TotalSeconds.Value} SECONDS;");
+  }
+}
