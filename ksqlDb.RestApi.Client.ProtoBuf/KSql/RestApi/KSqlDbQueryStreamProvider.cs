addbac688f604c596179c228cb84bdbcaba98229
Tomas Fabian <t.fabian@cid.com>
2022-08-05 19:57:52 +0200

    [ksqlDB.RestApi.Client.Protobuf]: KSqlDbQueryStreamProvider

---
+++ ksqlDb.RestApi.Client.ProtoBuf/KSql/RestApi/KSqlDbQueryStreamProvider.cs
@@ -0,0 +1,15 @@
+﻿using ksqlDb.RestApi.Client.KSql.Query.Context.Options;
+using Microsoft.Extensions.Logging;
+using IHttpClientFactory = ksqlDB.RestApi.Client.KSql.RestApi.Http.IHttpClientFactory;
+
+namespace ksqlDb.RestApi.Client.ProtoBuf.KSql.RestApi;
+
+internal class KSqlDbQueryStreamProvider : KSqlDbQueryProvider
+{
+  public KSqlDbQueryStreamProvider(IHttpClientFactory httpClientFactory, KSqlDbProviderOptions options, ILogger? logger = null) 
+    : base(httpClientFactory, options, logger)
+  {
+  }
+
+  protected override string QueryEndPointName => "query-stream";
+}
