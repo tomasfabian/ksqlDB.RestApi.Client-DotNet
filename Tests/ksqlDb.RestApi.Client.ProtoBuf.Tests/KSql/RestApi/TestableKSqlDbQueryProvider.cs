3215db86d6aaa7c4f186c03c3193f1bcec329eee
Tomas Fabian <t.fabian@cid.com>
2022-11-03 18:44:23 +0100

    [ksqlDB.RestApi.Client.ProtoBuf]: added TestableKSqlDbQueryProvider into ksqlDB.RestApi.Client.ProtoBuf unit tests project

--- Tests/ksqlDb.RestApi.Client.ProtoBuf.Tests/KSql/RestApi/TestableKSqlDbQueryProvider.cs
+++ Tests/ksqlDb.RestApi.Client.ProtoBuf.Tests/KSql/RestApi/TestableKSqlDbQueryProvider.cs
@@ -21,37 +21,16 @@ internal class TestableKSqlDbQueryProvider : KSqlDbQueryProvider
     : base(httpClientFactory, KSqlDbContextOptionsInstance)
   {
   }
-
-  protected string QueryResponse = "[{\"header\":{\"queryId\":\"_confluent-ksql-default_transient_9174388154324047204_1614627435343\",\"schema\":\"`ID` INTEGER, `ARR` ARRAY<STRUCT<`TITLE` STRING, `ID` INTEGER>>, `MAPVALUE` MAP<STRING, MAP<STRING, INTEGER>>, `MAPARR` MAP<INTEGER, ARRAY<STRING>>, `STR` STRUCT<`TITLE` STRING, `ID` INTEGER>, `RELEASE_YEAR` INTEGER\"}},\r\n{\"row\":{\"columns\":[1,[{\"TITLE\":\"Aliens\",\"ID\":1},{\"TITLE\":\"test\",\"ID\":2}],{\"a\":{\"a\":1,\"b\":2},\"b\":{\"d\":4,\"c\":3}},{\"1\":[\"a\",\"b\"],\"2\":[\"c\",\"d\"]},{\"TITLE\":\"Aliens\",\"ID\":1},1986]}},\r\n{\"row\":{\"columns\":[2,[{\"TITLE\":\"Die Hard\",\"ID\":2},{\"TITLE\":\"test\",\"ID\":2}],{\"a\":{\"a\":1,\"b\":2},\"b\":{\"d\":4,\"c\":3}},{\"1\":[\"a\",\"b\"],\"2\":[\"c\",\"d\"]},{\"TITLE\":\"Die Hard\",\"ID\":2},1998]}},";
+  protected string QueryResponse = "[{\"header\":{\"queryId\":\"transient_MOVIES_7139331784417618909\",\"schema\":\"`ID` INTEGER, `TITLE` STRING, `RELEASE_YEAR` INTEGER, `ROWTIME` BIGINT\",\"protoSchema\":\"syntax = \\\"proto3\\\";\\n\\nmessage ConnectDefault1 {\\n  int32 ID = 1;\\n  string TITLE = 2;\\n  int32 RELEASE_YEAR = 3;\\n  int64 ROWTIME = 4;\\n}\\n\"}},";
+  protected string ItemResponse = "{\"row\":{\"protobufBytes\":\"CAESBkFsaWVucxjCDyCrw6nKwzA=\"}},";
 
   protected override HttpClient OnCreateHttpClient()
   {     
-    return FakeHttpClient.CreateWithResponse(QueryResponse);;
+    return FakeHttpClient.CreateWithResponse(QueryResponse);
   }
 }
 public static class FakeHttpClient
 {
-  public static Mock<DelegatingHandler> CreateDelegatingHandler(string responseContent, HttpStatusCode statusCode = HttpStatusCode.OK)
-  {
-    var handlerMock = new Mock<DelegatingHandler>();
-
-    handlerMock
-      .Protected()
-      .Setup<Task<HttpResponseMessage>>(
-        nameof(HttpClient.SendAsync),
-        ItExpr.IsAny<HttpRequestMessage>(),
-        ItExpr.IsAny<CancellationToken>()
-      )
-      .ReturnsAsync(new HttpResponseMessage
-      {
-        StatusCode = statusCode,
-        Content = new StringContent(responseContent),
-      })
-      .Verifiable();
-
-    return handlerMock;
-  }
-
   public static Mock<HttpMessageHandler> CreateHttpMessageHandler(string responseContent, HttpStatusCode statusCode = HttpStatusCode.OK)
   {
     var handlerMock = new Mock<HttpMessageHandler>();
