e6418f5710d6c9c092e7014f72ce42591b2e9148
Tomas Fabian <t.fabian@cid.com>
2022-11-11 20:12:57 +0100

    [ksqlDB.RestApi.Client.ProtoBuf]: ProtoBufKSqlDbContext unit tests

---
+++ Tests/ksqlDb.RestApi.Client.ProtoBuf.Tests/KSql/Query/ProtoBufKSqlDbContextTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using ksqlDb.RestApi.Client.ProtoBuf.Tests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using UnitTests;
+using ksqlDB.RestApi.Client.KSql.Linq;
+
+namespace ksqlDb.RestApi.Client.ProtoBuf.Tests.KSql.Query;
+
+[TestClass]
+public class ProtoBufKSqlDbContextTests : TestBase
+{
+  [TestMethod]
+  public void CreateQueryStream_Subscribe_KSqlDbProvidersRunWasCalled()
+  {
+    //Arrange
+    var context = new TestableProtoBufKSqlDbContext<string>(TestParameters.KsqlDbUrl);
+
+    //Act
+    using var subscription = context.CreateQueryStream<string>()
+      .Subscribe(_ => { });
+
+    //Assert
+    subscription.Should().NotBeNull();
+    context.KSqlDbProviderMock.Verify(c => c.Run<string>(It.IsAny<object>(), It.IsAny<CancellationToken>()),
+      Times.Once);
+  }
+}
