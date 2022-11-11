24ef92b789253e861091191563932198ce86ebeb
Tomas Fabian <t.fabian@cid.com>
2022-11-11 19:36:27 +0100

    [ksqlDB.RestApi.Client.ProtoBuf]: added TestableProtoBufKSqlDbContext

---
+++ Tests/ksqlDb.RestApi.Client.ProtoBuf.Tests/KSql/Query/TestableProtoBufKSqlDbContext.cs
@@ -0,0 +1,49 @@
+﻿using ksqlDB.RestApi.Client.KSql.Query;
+using ksqlDB.RestApi.Client.KSql.Query.Context;
+using ksqlDB.RestApi.Client.KSql.RestApi;
+using ksqlDb.RestApi.Client.ProtoBuf.KSql.Query;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace ksqlDb.RestApi.Client.ProtoBuf.Tests.KSql.Query;
+
+public class TestableProtoBufKSqlDbContext<TValue> : ProtoBufKSqlDbContext
+{
+  public TestableProtoBufKSqlDbContext(string ksqlDbUrl)
+    : base(ksqlDbUrl)
+  {
+    InitMocks();
+  }
+
+  public TestableProtoBufKSqlDbContext(KSqlDBContextOptions contextOptions) : base(contextOptions)
+  {
+    InitMocks();
+  }
+
+  private void InitMocks()
+  {
+    KSqlDbProviderMock.Setup(c => c.Run<TValue>(It.IsAny<object>(), It.IsAny<CancellationToken>()))
+      .Returns(GetAsyncEnumerable);
+  }
+
+  protected virtual IAsyncEnumerable<TValue> GetAsyncEnumerable()
+  {
+    return new List<TValue>().ToAsyncEnumerable();
+  }
+
+  public readonly Mock<IKSqlDbProvider> KSqlDbProviderMock = new();
+  public readonly Mock<IKSqlQueryGenerator> KSqlQueryGenerator = new();
+  public readonly Mock<IKSqlDbRestApiClient> KSqlDbRestApiClientMock = new();
+
+  internal bool RegisterKSqlQueryGenerator { get; set; } = true;
+  protected override void OnConfigureServices(IServiceCollection serviceCollection, KSqlDBContextOptions options)
+  {
+    serviceCollection.AddSingleton(KSqlDbProviderMock.Object);
+    serviceCollection.AddSingleton(KSqlDbRestApiClientMock.Object);
+
+    if (RegisterKSqlQueryGenerator)
+      serviceCollection.AddSingleton(KSqlQueryGenerator.Object);
+
+    base.OnConfigureServices(serviceCollection, options);
+  }
+}
