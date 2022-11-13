d1e2834ae4b925ba9fe3679fddb7347d724cb72b
Tomas Fabian <t.fabian@cid.com>
2022-11-13 11:41:46 +0100

    [ksqlDB.RestApi.Client]: RowValueJsonSerializer unit tests

--- Tests/ksqlDB.RestApi.Client.Tests/KSql/RestApi/RowValueJsonSerializerTests.cs
+++ Tests/ksqlDB.RestApi.Client.Tests/KSql/RestApi/RowValueJsonSerializerTests.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 using ksqlDb.RestApi.Client.KSql.Query.Context.Options;
 using ksqlDB.RestApi.Client.KSql.RestApi;
 using ksqlDB.RestApi.Client.KSql.RestApi.Responses;
@@ -19,8 +19,8 @@ public class RowValueJsonSerializerTests : TestBase
 
     var queryStreamHeader = new QueryStreamHeader()
     {
-      ColumnTypes = new []{ "STRING" },
-      ColumnNames = new []{ "NAME" },
+      ColumnTypes = new[] { "STRING" },
+      ColumnNames = new[] { "NAME" },
     };
 
     ClassUnderTest = new RowValueJsonSerializer(queryStreamHeader);
@@ -36,6 +36,14 @@ public class RowValueJsonSerializerTests : TestBase
   public void Deserialize_RecordWithSingleProperty()
   {
     //Arrange
+    var queryStreamHeader = new QueryStreamHeader()
+    {
+      ColumnTypes = new[] { "STRING" },
+      ColumnNames = new[] { "NAME" },
+    };
+
+    ClassUnderTest = new RowValueJsonSerializer(queryStreamHeader);
+
     string rawJson = "[\"f03c278c-61ea-4f69-b153-5647d2eec72e\"]";
     var jsonSerializationOptions = KSqlDbJsonSerializerOptions.CreateInstance();
 
@@ -50,6 +58,14 @@ public class RowValueJsonSerializerTests : TestBase
   public void Deserialize_String()
   {
     //Arrange
+    var queryStreamHeader = new QueryStreamHeader()
+    {
+      ColumnTypes = new[] { "STRING" },
+      ColumnNames = new[] { "KSQL_COL_0" },
+    };
+
+    ClassUnderTest = new RowValueJsonSerializer(queryStreamHeader);
+
     string rawJson = "[\"f03c278c-61ea-4f69-b153-5647d2eec72e\"]";
     var jsonSerializationOptions = KSqlDbJsonSerializerOptions.CreateInstance();
 
@@ -61,9 +77,17 @@ public class RowValueJsonSerializerTests : TestBase
   }
 
   [TestMethod]
-  public void Deserialize_Struct()
+  public void Deserialize_DateTimeOffsetStruct()
   {
     //Arrange
+    var queryStreamHeader = new QueryStreamHeader()
+    {
+      ColumnTypes = new[] { "STRING" },
+      ColumnNames = new[] { "KSQL_COL_0" },
+    };
+
+    ClassUnderTest = new RowValueJsonSerializer(queryStreamHeader);
+
     var dateTime = new DateTimeOffset(new DateTime(2022, 9, 23), TimeSpan.FromHours(2));
     var dt = "2022-09-23T00:00:00+02:00";
 
@@ -78,9 +102,41 @@ public class RowValueJsonSerializerTests : TestBase
   }
 
   [TestMethod]
+  public void Deserialize_Dictionary()
+  {
+    //Arrange
+    var queryStreamHeader = new QueryStreamHeader()
+    {
+      ColumnTypes = new[] { "MAP<STRING, INTEGER>" },
+      ColumnNames = new[] { "KSQL_COL_0" },
+    };
+
+    ClassUnderTest = new RowValueJsonSerializer(queryStreamHeader);
+
+    var value = "{\"a\":1,\"b\":2}";
+
+    string rawJson = $"[{value}]";
+    var jsonSerializationOptions = KSqlDbJsonSerializerOptions.CreateInstance();
+
+    //Act
+    var rowValue = ClassUnderTest.Deserialize<Dictionary<string, int>>(rawJson, jsonSerializationOptions);
+
+    //Assert
+    rowValue.Value["a"].Should().Be(1);
+  }
+
+  [TestMethod]
   public void Deserialize_PrimitiveInt()
   {
     //Arrange
+    var queryStreamHeader = new QueryStreamHeader()
+    {
+      ColumnTypes = new[] { "INTEGER" },
+      ColumnNames = new[] { "KSQL_COL_0" },
+    };
+
+    ClassUnderTest = new RowValueJsonSerializer(queryStreamHeader);
+
     var value = 42;
 
     string rawJson = $"[{value}]";
@@ -103,6 +159,14 @@ public class RowValueJsonSerializerTests : TestBase
   public void Deserialize_MyStruct()
   {
     //Arrange
+    var queryStreamHeader = new QueryStreamHeader()
+    {
+      ColumnTypes = new[] { "STRUCT<`NAME` STRING>" },
+      ColumnNames = new[] { "KSQL_COL_0" },
+    };
+
+    ClassUnderTest = new RowValueJsonSerializer(queryStreamHeader);
+
     string value = "E.T.";
     string rawJson = $"[\"{value}\"]";
     var jsonSerializationOptions = KSqlDbJsonSerializerOptions.CreateInstance();
