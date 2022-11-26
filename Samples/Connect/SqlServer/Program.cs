ca3e6cc80b1c6e569433689e883bd05dc0a8e494
Tomas Fabian <t.fabian@cid.com>
2022-11-26 11:47:31 +0100

    [ksqlDB.RestApi.Client]: SqlServer Connect example improvements

--- Samples/Connect/SqlServer/Program.cs
+++ Samples/Connect/SqlServer/Program.cs
@@ -24,22 +24,25 @@ namespace Connect.SqlServer
       var connectRestApiClient = new ConnectRestApiClient(httpClientFactory);
 	
       var httpResponseMessage = await connectRestApiClient.PostConnectorAsync(connectorMetadata, connectorName);
+
+      var message = await httpResponseMessage.Content.ReadAsStringAsync();
+
+      Console.WriteLine(message);
     }
 
-    static string connectionString = @"Server=sqlserver2019,1433;User Id = SA;Password=<YourNewStrong@Passw0rd>;Initial Catalog = Sensors;MultipleActiveResultSets=true";
+    static readonly string ConnectionString = @"Server=sqlserver2019,1433;User Id = SA;Password=<YourNewStrong@Passw0rd>;Initial Catalog = Sensors;MultipleActiveResultSets=true";
+    static readonly string BootstrapServers = "localhost:29092";
+    static readonly string TableName = "Sensors";
+    static readonly string SchemaName = "dbo";
 
-    static string bootstrapServers = "localhost:29092";
-
-    static string tableName = "Sensors";
-    static string schemaName = "dbo";
     private static SqlServerConnectorMetadata CreateConnectorMetadata()
     {
-      var createConnector = new SqlServerConnectorMetadata(connectionString)
-        .SetTableIncludeListPropertyName($"{schemaName}.{tableName}")
+      var createConnector = new SqlServerConnectorMetadata(ConnectionString)
+        .SetTableIncludeListPropertyName($"{SchemaName}.{TableName}")
         .SetJsonKeyConverter()
         .SetJsonValueConverter()
-        .SetProperty("database.history.kafka.bootstrap.servers", bootstrapServers)
-        .SetProperty("database.history.kafka.topic", $"dbhistory.{tableName}")
+        .SetProperty("database.history.kafka.bootstrap.servers", BootstrapServers)
+        .SetProperty("database.history.kafka.topic", $"dbhistory.{TableName}")
         .SetProperty("database.server.name", "sqlserver2019")
         .SetProperty("key.converter.schemas.enable", "false")
         .SetProperty("value.converter.schemas.enable", "false")
