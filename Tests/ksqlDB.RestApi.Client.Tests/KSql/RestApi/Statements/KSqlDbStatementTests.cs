9da111cd8cf532e3e378e21160b3f94c428c2297
Tomas Fabian <t.fabian@cid.com>
2022-12-23 17:31:56 +0100

    [ksqlDB.RestApi.Client]: added shared values into KSqlDbStatement unit test

--- Tests/ksqlDB.RestApi.Client.Tests/KSql/RestApi/Statements/KSqlDbStatementTests.cs
+++ Tests/ksqlDB.RestApi.Client.Tests/KSql/RestApi/Statements/KSqlDbStatementTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+using System.Text;
+using System.Text.Json;
 using FluentAssertions;
 using ksqlDB.RestApi.Client.KSql.RestApi.Statements;
 using NUnit.Framework;
@@ -47,5 +48,21 @@ public class KSqlDbStatementTests : TestBase
 
     //Assert
     statementText.Should().Be(Statement);
+  }
+
+  [Test]
+  public void SessionVariables()
+  {
+    //Arrange
+    var ksqlDbStatement = new KSqlDbStatement(Statement);
+
+    ksqlDbStatement.SessionVariables.Add("key1", "value1");
+    ksqlDbStatement.SessionVariables.Add("key2", "value2");
+
+    //Act
+    var json = JsonSerializer.Serialize(ksqlDbStatement);
+
+    //Assert
+    json.Should().Contain(@"""sessionVariables"":{""key1"":""value1"",""key2"":""value2""}");
   }
 }
