4cb82d84f1dbe845e83ff625d61cd8c3b578c4e7
Tomas Fabian <t.fabian@cid.com>
2023-07-06 07:40:13 +0200

    [ksqlDB.RestApi.Client]: added xml doc comments for DatabaseChangeObject type

--- Samples/Aggregations/Model/DatabaseChangeObject.cs
+++ Samples/Aggregations/Model/DatabaseChangeObject.cs
@@ -6,16 +6,30 @@ public record IoTSensorChange : DatabaseChangeObject<IoTSensor>
 {
 }
 
+/// <summary>
+/// Represents a generic database change object.
+/// </summary>
+/// <typeparam name="TEntity">A generic type parameter TEntity, which represents the entity type associated with the database change.</typeparam>
 public record DatabaseChangeObject<TEntity> : DatabaseChangeObject
 {
+  /// <summary>
+  /// Represents the state of the entity before the change occurred.
+  /// </summary>
   public TEntity? Before { get; set; }
+  /// <summary>
+  /// Represents the state of the entity after the change occurred.
+  /// </summary>
   public TEntity? After { get; set; }
   public TEntity? EntityBefore => Before;
   public TEntity? EntityAfter => After;
 }
 
+/// <summary>
+/// Represents a generic database change object.
+/// </summary>
 public record DatabaseChangeObject
 {
+  // Represents the operation performed on the database (e.g., "insert," "update," or "delete").
   public string Op { get; set; } = null!;
   public long? TsMs { get; set; }
 }
--- SqlServer.Connector/Cdc/DatabaseChangeObject.cs
+++ SqlServer.Connector/Cdc/DatabaseChangeObject.cs
@@ -1,16 +1,31 @@
-﻿using SqlServer.Connector.Cdc.Extensions;
+using SqlServer.Connector.Cdc.Extensions;
 
 namespace SqlServer.Connector.Cdc
 {
+  /// <summary>
+  /// Represents a generic database change object.
+  /// </summary>
+  /// <typeparam name="TEntity">A generic type parameter TEntity, which represents the entity type associated with the database change.</typeparam>
   public record DatabaseChangeObject<TEntity> : DatabaseChangeObject, IDatabaseChangeObject<TEntity>
   {
+    /// <summary>
+    /// Represents the state of the entity before the change occurred.
+    /// </summary>
     public TEntity Before { get; set; }
+
+    /// <summary>
+    /// Represents the state of the entity after the change occurred.
+    /// </summary>
     public TEntity After { get; set; }
   }
 
+  /// <summary>
+  /// Represents a generic database change object.
+  /// </summary>
   public record DatabaseChangeObject
   {
     public Source Source { get; set; }
+    // Represents the operation performed on the database (e.g., "insert," "update," or "delete").
     public string Op { get; set; }
     public long TsMs { get; set; }
     public object Transaction { get; set; }
