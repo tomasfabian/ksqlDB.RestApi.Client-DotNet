c660e1bfa643d23f4c9be3b69e3a5a8c4a7c4471
Tomas Fabian <t.fabian@cid.com>
2023-07-14 19:36:42 +0200

    [ksqlDB.RestApi.Client]: added doc strings to TimeWindows

--- ksqlDb.RestApi.Client/KSql/Query/Windows/TimeWindows.cs
+++ ksqlDb.RestApi.Client/KSql/Query/Windows/TimeWindows.cs
@@ -1,9 +1,17 @@
-﻿using ksqlDb.RestApi.Client.KSql.Query.PushQueries;
+using ksqlDb.RestApi.Client.KSql.Query.PushQueries;
 
 namespace ksqlDB.RestApi.Client.KSql.Query.Windows;
 
+/// <summary>
+/// Represents time windows for time-based operations.
+/// </summary>
 public class TimeWindows
 {
+  /// <summary>
+  /// Initializes a new instance of the <see cref="TimeWindows"/> class with the specified duration and output refinement.
+  /// </summary>
+  /// <param name="duration">The duration of the time window.</param>
+  /// <param name="outputRefinement">The output refinement mode.</param>
   public TimeWindows(Duration duration, OutputRefinement outputRefinement = OutputRefinement.Emit)
   {
     Duration = duration ?? throw new ArgumentNullException(nameof(duration));
@@ -11,10 +19,27 @@ public class TimeWindows
     OutputRefinement = outputRefinement;
   }
 
+  /// <summary>
+  /// Gets the duration of the time window.
+  /// </summary>
   public Duration Duration { get; }
+
+  /// <summary>
+  /// Gets the grace period for the time window.
+  /// </summary>
   public Duration GracePeriod { get; private set; }
+
+  /// <summary>
+  /// Gets the output refinement mode of the time window.
+  /// </summary>
   public OutputRefinement OutputRefinement { get; }
 
+  /// <summary>
+  /// Sets the grace period for the time window.
+  /// </summary>
+  /// <param name="gracePeriod">The grace period duration.</param>
+  /// <returns>The updated <see cref="TimeWindows"/> object.</returns>
+  /// <exception cref="ArgumentNullException">Thrown when <paramref name="gracePeriod"/> is null.</exception>
   public TimeWindows WithGracePeriod(Duration gracePeriod)
   {
     GracePeriod = gracePeriod ?? throw new ArgumentNullException(nameof(gracePeriod));
