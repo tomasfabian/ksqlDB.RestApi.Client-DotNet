3e32a0b1630020e45199f516af6a09c554597053
Tomas Fabian <t.fabian@cid.com>
2023-07-14 18:39:08 +0200

    [ksqlDB.RestApi.Client]: Duration added docstrings to Duration class

--- ksqlDb.RestApi.Client/KSql/Query/Windows/Duration.cs
+++ ksqlDb.RestApi.Client/KSql/Query/Windows/Duration.cs
@@ -1,19 +1,39 @@
-﻿namespace ksqlDB.RestApi.Client.KSql.Query.Windows;
+namespace ksqlDB.RestApi.Client.KSql.Query.Windows;
 
+/// <summary>
+/// Represents a duration of time.
+/// </summary>
 public class Duration
 {
+  /// <summary>
+  /// Gets the time unit of the duration.
+  /// </summary>
   public TimeUnits TimeUnit { get; private set; }
+
+  /// <summary>
+  /// Gets the value of the duration.
+  /// </summary>
   public uint Value { get; private set; }
 
-  public static Duration OfMilliseconds(uint seconds)
+  /// <summary>
+  /// Creates a new <see cref="Duration"/> object with the specified value in milliseconds.
+  /// </summary>
+  /// <param name="milliseconds">The value in milliseconds.</param>
+  /// <returns>A new <see cref="Duration"/> object.</returns>
+  public static Duration OfMilliseconds(uint milliseconds)
   {
     return new()
     {
       TimeUnit = TimeUnits.MILLISECONDS,
-      Value = seconds
+      Value = milliseconds
     };
   }
 
+  /// <summary>
+  /// Creates a new <see cref="Duration"/> object with the specified value in seconds.
+  /// </summary>
+  /// <param name="seconds">The value in seconds.</param>
+  /// <returns>A new <see cref="Duration"/> object.</returns>
   public static Duration OfSeconds(uint seconds)
   {
     return new()
@@ -23,6 +43,11 @@ public class Duration
     };
   }
 
+  /// <summary>
+  /// Creates a new <see cref="Duration"/> object with the specified value in minutes.
+  /// </summary>
+  /// <param name="minutes">The value in minutes.</param>
+  /// <returns>A new <see cref="Duration"/> object.</returns>
   public static Duration OfMinutes(uint minutes)
   {
     return new()
@@ -32,6 +57,11 @@ public class Duration
     };
   }
 
+  /// <summary>
+  /// Creates a new <see cref="Duration"/> object with the specified value in hours.
+  /// </summary>
+  /// <param name="hours">The value in hours.</param>
+  /// <returns>A new <see cref="Duration"/> object.</returns>
   public static Duration OfHours(uint hours)
   {
     return new()
@@ -41,6 +71,11 @@ public class Duration
     };
   }
 
+  /// <summary>
+  /// Creates a new <see cref="Duration"/> object with the specified value in days.
+  /// </summary>
+  /// <param name="days">The value in days.</param>
+  /// <returns>A new <see cref="Duration"/> object.</returns>
   public static Duration OfDays(uint days)
   {
     return new()
@@ -50,6 +85,9 @@ public class Duration
     };
   }
 
+  /// <summary>
+  /// Gets the total duration in seconds.
+  /// </summary>
   public Duration TotalSeconds
   {
     get
@@ -63,7 +101,7 @@ public class Duration
         _ => throw new ArgumentOutOfRangeException()
       };
 
-      return Duration.OfSeconds(totalSeconds);
+      return OfSeconds(totalSeconds);
     }
   }
 }
