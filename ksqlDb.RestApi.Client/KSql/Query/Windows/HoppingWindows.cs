692a19836a67b6b4aaa1b3ea59ac900f0d3e594f
Tomas Fabian <t.fabian@cid.com>
2023-07-14 19:33:48 +0200

    [ksqlDB.RestApi.Client]: added doc strings to HoppingWindows

--- ksqlDb.RestApi.Client/KSql/Query/Windows/HoppingWindows.cs
+++ ksqlDb.RestApi.Client/KSql/Query/Windows/HoppingWindows.cs
@@ -1,17 +1,35 @@
-﻿using ksqlDb.RestApi.Client.KSql.Query.PushQueries;
+using ksqlDb.RestApi.Client.KSql.Query.PushQueries;
 
 namespace ksqlDB.RestApi.Client.KSql.Query.Windows;
 
+/// <summary>
+/// Represents hopping windows for time-based operations.
+/// </summary>
 public class HoppingWindows : TimeWindows
 {
+  /// <summary>
+  /// Initializes a new instance of the <see cref="HoppingWindows"/> class with the specified duration and output refinement.
+  /// </summary>
+  /// <param name="duration">The duration of the hopping window.</param>
+  /// <param name="outputRefinement">The output refinement mode.</param>
   public HoppingWindows(Duration duration, OutputRefinement outputRefinement = OutputRefinement.Emit) 
     : base(duration, outputRefinement)
   {
     AdvanceBy = duration;
-  }    
+  }
 
+  /// <summary>
+  /// Gets or sets the interval by which the window advances.
+  /// </summary>
   public Duration AdvanceBy { get; private set; }
 
+  /// <summary>
+  /// Sets the advancement interval for the hopping window.
+  /// </summary>
+  /// <param name="advanceBy">The interval by which the window advances.</param>
+  /// <returns>The updated <see cref="HoppingWindows"/> object.</returns>
+  /// <exception cref="ArgumentNullException">Thrown when <paramref name="advanceBy"/> is null.</exception>
+  /// <exception cref="InvalidOperationException">Thrown when the window advancement interval is greater than the window duration.</exception>
   public HoppingWindows WithAdvanceBy(Duration advanceBy)
   {
     AdvanceBy = advanceBy ?? throw new ArgumentNullException(nameof(advanceBy));
@@ -22,8 +40,17 @@ public class HoppingWindows : TimeWindows
     return this;
   }
 
+  /// <summary>
+  /// Gets the retention duration for the hopping window.
+  /// </summary>
   public Duration Retention { get; private set; }
 
+  /// <summary>
+  /// Sets the retention duration for the hopping window.
+  /// </summary>
+  /// <param name="retention">The retention duration.</param>
+  /// <returns>The updated <see cref="HoppingWindows"/> object.</returns>
+  /// <exception cref="ArgumentNullException">Thrown when <paramref name="retention"/> is null.</exception>
   public HoppingWindows WithRetention(Duration retention)
   {
     Retention = retention ?? throw new ArgumentNullException(nameof(retention));
