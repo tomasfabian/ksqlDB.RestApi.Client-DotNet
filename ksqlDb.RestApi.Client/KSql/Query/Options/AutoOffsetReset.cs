7d04fd312edc2057dc89974639a638c8e542794a
Tomas Fabian <t.fabian@cid.com>
2023-07-16 10:09:40 +0200

    [ksqlDB.RestApi.Client]: added doc strings to AutoOffsetReset

--- ksqlDb.RestApi.Client/KSql/Query/Options/AutoOffsetReset.cs
+++ ksqlDb.RestApi.Client/KSql/Query/Options/AutoOffsetReset.cs
@@ -1,7 +1,19 @@
-﻿namespace ksqlDB.RestApi.Client.KSql.Query.Options;
+namespace ksqlDB.RestApi.Client.KSql.Query.Options;
 
-public enum AutoOffsetReset 
+/// <summary>
+/// Kafka offset reset policies define how a consumer should handle situations where it tries to read from a partition in Kafka but there is no valid offset available or the offset is out of range.
+/// </summary>
+public enum AutoOffsetReset
 {
+  /// <summary>
+  /// Automatically reset the offset to the earliest available offset.
+  /// This will be applied when a consumer group has no committed offset.
+  /// </summary>
   Earliest,
+
+  /// <summary>
+  /// If the consumer requests an invalid offset or an offset that is out of range, it will reset to the earliest offset available in the partition.
+  /// The consumer will then start consuming messages from the beginning of the partition, including any messages that were produced before the consumer started.
+  /// </summary>
   Latest
 }
--- ksqlDb.RestApi.Client/KSql/Query/Options/AutoOffsetResetExtensions.cs
+++ ksqlDb.RestApi.Client/KSql/Query/Options/AutoOffsetResetExtensions.cs
@@ -1,7 +1,16 @@
-﻿namespace ksqlDB.RestApi.Client.KSql.Query.Options;
+namespace ksqlDB.RestApi.Client.KSql.Query.Options;
 
+/// <summary>
+/// Provides extension methods for converting string values to <see cref="AutoOffsetReset"/> and vice versa.
+/// </summary>
 public static class AutoOffsetResetExtensions
 {
+  /// <summary>
+  /// Converts a string value to <see cref="AutoOffsetReset"/>.
+  /// </summary>
+  /// <param name="autoOffsetResetValue">The string value representing the auto offset reset policy.</param>
+  /// <returns>The corresponding <see cref="AutoOffsetReset"/> value.</returns>
+  /// <exception cref="ArgumentOutOfRangeException">Thrown when the provided <paramref name="autoOffsetResetValue"/> is not a valid option.</exception>
   public static AutoOffsetReset ToAutoOffsetReset(this string autoOffsetResetValue)
   {        
     if (autoOffsetResetValue == "earliest")
@@ -13,6 +22,11 @@ public static class AutoOffsetResetExtensions
     throw new ArgumentOutOfRangeException(nameof(autoOffsetResetValue), autoOffsetResetValue, null);
   }
 
+  /// <summary>
+  /// Converts an <see cref="AutoOffsetReset"/> value to its corresponding string representation for KSql.
+  /// </summary>
+  /// <param name="value">The <see cref="AutoOffsetReset"/> value.</param>
+  /// <returns>The string representation of the <see cref="AutoOffsetReset"/> value for KSql.</returns>
   public static string ToKSqlValue(this AutoOffsetReset value)
   {
     return value.ToString().ToLower();
