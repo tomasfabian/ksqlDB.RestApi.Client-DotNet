2b939fac5bad81909bcce389ed2784eadcc63c4d
Tomas Fabian <t.fabian@cid.com>
2023-07-16 10:15:20 +0200

    [ksqlDB.RestApi.Client]: added doc strings to QueryType

--- ksqlDb.RestApi.Client/KSql/Query/Options/QueryType.cs
+++ ksqlDb.RestApi.Client/KSql/Query/Options/QueryType.cs
@@ -1,7 +1,17 @@
-﻿namespace ksqlDB.RestApi.Client.KSql.Query.Options;
+namespace ksqlDB.RestApi.Client.KSql.Query.Options;
 
+/// <summary>
+/// Specifies KSQL query endpoints when using the ksqlDB REST API.
+/// </summary>
 public enum QueryType
 {
+  /// <summary>
+  /// Represents the "/ksql" endpoint in the ksqlDB REST API.
+  /// </summary>
   Query,
+
+  /// <summary>
+  /// Represents the "/query-stream" endpoint in the ksqlDB REST API.
+  /// </summary>
   QueryStream
 }
