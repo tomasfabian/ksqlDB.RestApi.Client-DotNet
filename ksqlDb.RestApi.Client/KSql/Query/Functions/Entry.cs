20c8b38cbeb78eb41c790a02a6b89eeb095baf7d
Tomas Fabian <t.fabian@cid.com>
2023-07-19 07:51:11 +0200

    [ksqlDB.RestApi.Client]: added doc strings to Entry

--- ksqlDb.RestApi.Client/KSql/Query/Functions/Entry.cs
+++ ksqlDb.RestApi.Client/KSql/Query/Functions/Entry.cs
@@ -1,7 +1,18 @@
-﻿namespace ksqlDB.RestApi.Client.KSql.Query.Functions;
+namespace ksqlDB.RestApi.Client.KSql.Query.Functions;
 
+/// <summary>
+/// Represents an entry with a key and a value.
+/// </summary>
+/// <typeparam name="TValue">The type of the value.</typeparam>
 public struct Entry<TValue>
 {
+  /// <summary>
+  /// Gets or sets the key of the entry.
+  /// </summary>
   public string K { get; set; }
+
+  /// <summary>
+  /// Gets or sets the value of the entry.
+  /// </summary>
   public TValue V { get; set; }
 }
