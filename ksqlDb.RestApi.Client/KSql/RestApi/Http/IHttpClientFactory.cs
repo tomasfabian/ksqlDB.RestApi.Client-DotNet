08b4189610e08fd96191b1086194cda931bbb091
Tomas Fabian <t.fabian@cid.com>
2023-08-07 17:28:08 +0200

    [ksqlDB.RestApi.Client]: added doc strings for IHttpClientFactory

--- ksqlDb.RestApi.Client/KSql/RestApi/Http/IHttpClientFactory.cs
+++ ksqlDb.RestApi.Client/KSql/RestApi/Http/IHttpClientFactory.cs
@@ -1,6 +1,14 @@
-﻿namespace ksqlDB.RestApi.Client.KSql.RestApi.Http;
+namespace ksqlDB.RestApi.Client.KSql.RestApi.Http;
 
+/// <summary>
+/// Defines a factory for creating instances of <see cref="HttpClient"/>.
+/// </summary>
 public interface IHttpClientFactory
 {
+  /// <summary>
+  /// Creates a new instance of <see cref="HttpClient"/>.
+  /// </summary>
+  /// <returns>A new instance of <see cref="HttpClient"/>.</returns>
+
   HttpClient CreateClient();
 }
