5e02510d3fd48108f3724c8cc2e25b48101d78dc
Tomas Fabian <t.fabian@cid.com>
2023-09-17 09:42:38 +0200

    [ksqlDB.RestApi.Client]: NewVisitor tests

---
+++ Tests/ksqlDB.RestApi.Client.Tests/KSql/Query/Visitors/NewVisitorTests.cs
@@ -0,0 +1,67 @@
+using System.Linq.Expressions;
+using System.Text;
+using FluentAssertions;
+using ksqlDB.Api.Client.Tests.Models;
+using ksqlDB.RestApi.Client.KSql.Linq;
+using ksqlDB.RestApi.Client.KSql.Query.Visitors;
+using NUnit.Framework;
+
+namespace ksqlDB.Api.Client.Tests.KSql.Query.Visitors
+{
+  public class NewVisitorTests
+  {
+    private StringBuilder stringBuilder = null!;
+    private KSqlQueryMetadata queryMetadata = null!;
+    private NewVisitor newVisitor = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+      stringBuilder = new StringBuilder();
+      queryMetadata = new KSqlQueryMetadata();
+      newVisitor = new NewVisitor(stringBuilder, queryMetadata);
+    }
+
+    [Test]
+    public void PropertyWithAliasAndCountAggregateFunction_VisitNew()
+    {
+      //Arrange
+      Expression<Func<IKSqlGrouping<int, Location>, object>> expression = l => new { Key = l.Key, Agg = l.Count() };
+
+      //Act
+      newVisitor.Visit(expression);
+
+      //Assert
+      var ksql = stringBuilder.ToString();
+      ksql.Should().Be("Key, COUNT(*) Agg");
+    }
+
+    [Test]
+    public void CountWithLambda_VisitNew()
+    {
+      //Arrange
+      Expression<Func<IKSqlGrouping<int, Location>, object>> expression = l => new { l.Key, Agg = l.Count(x => x.Longitude) };
+
+      //Act
+      newVisitor.Visit(expression);
+
+      //Assert
+      var ksql = stringBuilder.ToString();
+      ksql.Should().BeEquivalentTo($"Key, Count({nameof(Location.Longitude)}) Agg");
+    }
+
+    [Test]
+    public void Constructor()
+    {
+      //Arrange
+      Expression<Func<object>> expression = () => new { Loc = new Location { Latitude = "1" } };
+
+      //Act
+      newVisitor.Visit(expression);
+
+      //Assert
+      var ksql = stringBuilder.ToString();
+      ksql.Should().BeEquivalentTo("STRUCT(Latitude := '1') Loc");
+    }
+  }
+}
