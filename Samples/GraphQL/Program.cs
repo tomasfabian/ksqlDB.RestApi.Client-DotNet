0f7d6667e3af48df214beccfd5733e7ec6941bff
Tomas Fabian <t.fabian@cid.com>
2024-02-11 16:52:42 +0100

    [ksqlDB.RestApi.Client]: added GraphQL end point for subscriptions

--- Samples/GraphQL/Program.cs
+++ Samples/GraphQL/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddGraphQLServer();
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -13,5 +13,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGraphQL();
 
 app.Run();
