b4b498291b90d69c08e13f9d275fe12caaa23ab5
Tomas Fabian <t.fabian@cid.com>
2024-02-12 16:03:25 +0100

    [GraphQL.Sample]: added GraphQL configuration

--- Samples/GraphQL/Program.cs
+++ Samples/GraphQL/Program.cs
@@ -1,7 +1,15 @@
+using GraphQL.Movies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddGraphQLServer();
+builder.Services.AddGraphQLServer()
+  .AddInMemorySubscriptions()
+  .AddQueryType()
+  .AddTypeExtension<Queries>()
+  .AddSubscriptionType()
+  .AddTypeExtension<Subscription>();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -10,9 +18,12 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
 app.MapControllers();
 app.MapGraphQL();
+app.UseWebSockets();
 
 app.Run();
