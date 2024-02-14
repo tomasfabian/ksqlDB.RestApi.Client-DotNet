d9c3f1e7f0803fc29eee10b0263c0911f35712dd
Tomas Fabian <t.fabian@cid.com>
2024-02-14 19:23:25 +0100

    [GraphQL.Sample]: added KSqlDb configuration

--- Samples/GraphQL/Program.cs
+++ Samples/GraphQL/Program.cs
@@ -1,4 +1,8 @@
+using GraphQL.ksqlDB;
 using GraphQL.Movies;
+using GraphQL.Services;
+using ksqlDb.RestApi.Client.DependencyInjection;
+using ksqlDB.RestApi.Client.KSql.Query.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +13,19 @@ builder.Services.AddGraphQLServer()
   .AddTypeExtension<Queries>()
   .AddSubscriptionType()
   .AddTypeExtension<Subscription>();
+
+var ksqlDbUrl = "http://localhost:8088";
+
+builder.Services.AddDbContext<IMoviesKSqlDbContext, MoviesKSqlDbContext>(
+  options =>
+  {
+    var setupParameters = options.UseKSqlDb(ksqlDbUrl);
+
+    setupParameters.SetAutoOffsetReset(AutoOffsetReset.Earliest);
+
+  }, contextLifetime: ServiceLifetime.Transient, restApiLifetime: ServiceLifetime.Transient);
+
+builder.Services.AddHostedService<MoviesConsumerBackgroundService>();
 
 builder.Services.AddControllers();
 
