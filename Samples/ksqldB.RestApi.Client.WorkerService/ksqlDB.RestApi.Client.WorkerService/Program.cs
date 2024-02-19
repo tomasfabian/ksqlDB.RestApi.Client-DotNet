0aaa248e3cf41b348302d0998e341b27b797d20c
Tomas Fabian <t.fabian@cid.com>
2024-02-19 07:42:40 +0100

    [WorkerService.Sample]: added ksqlDbUrl to appsettings.json

--- Samples/ksqldB.RestApi.Client.WorkerService/ksqlDB.RestApi.Client.WorkerService/Program.cs
+++ Samples/ksqldB.RestApi.Client.WorkerService/ksqlDB.RestApi.Client.WorkerService/Program.cs
@@ -3,11 +3,12 @@ using ksqlDb.RestApi.Client.DependencyInjection;
 using ksqlDB.RestApi.Client.WorkerService;
 using ksqlDB.RestApi.Client.WorkerService.ksqlDB;
 
+// use command ksqlDB.RestApi.Client-DotNet\Samples\ksqlDB.RestApi.Client.Sample> docker compose up -d
+var ksqlDbUrl = Configuration().GetValue<string>("ksqlDbUrl") ?? throw new Exception("ksqlDbUrl is missing in appsettings.json");
+
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-      var ksqlDbUrl = @"http://ksqldb-server:8088"; // use command ksqlDB.RestApi.Client-DotNet\Samples\ksqlDB.RestApi.Client.Sample> docker compose up -d
-
       services.AddDbContext<IMoviesKSqlDbContext, MoviesKSqlDbContext>(
         options =>
         {
@@ -22,3 +23,11 @@ IHost host = Host.CreateDefaultBuilder(args)
     .Build();
 
 await host.RunAsync();
+
+IConfiguration Configuration()
+{
+  return new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+}
