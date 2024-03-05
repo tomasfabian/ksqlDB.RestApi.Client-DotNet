b408a029bfca73d78527811ff822096af0ee7bda
Tomas Fabian <t.fabian@cid.com>
2024-03-05 10:22:38 +0100

    [ksqlDB.RestApi.Client]: added doc strings for WindowType enum

--- ksqlDb.RestApi.Client/KSql/RestApi/Enums/WindowType.cs
+++ ksqlDb.RestApi.Client/KSql/RestApi/Enums/WindowType.cs
@@ -1,8 +1,22 @@
-﻿namespace ksqlDB.RestApi.Client.KSql.RestApi.Enums;
+namespace ksqlDB.RestApi.Client.KSql.RestApi.Enums;
 
+/// <summary>
+/// Represents different windowing types in ksqlDB that have distinct time boundaries.
+/// </summary>
 public enum WindowType
 {
+  /// <summary>
+  /// Represents a session window combines records into a session, delineating periods of activity separated by a defined duration of inactivity, known as "idleness".
+  /// </summary>
   Session,
+
+  /// <summary>
+  /// Represents a hopping window where events are grouped into fixed-size, possibly overlapping, time intervals.
+  /// </summary>
   Hopping,
+
+  /// <summary>
+  /// Represents a tumbling window where events are grouped into fixed-size, non-overlapping, time intervals.
+  /// </summary>
   Tumbling
 }
