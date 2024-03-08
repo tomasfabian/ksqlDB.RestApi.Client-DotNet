f296fd1f81c7827ec56864a5799c69efb7d5339a
Tomas Fabian <t.fabian@cid.com>
2024-03-08 10:24:42 +0100

    [ksqlDb.RestApi.Client]: fixed KSqlDbRestApiClient integration tests

---
+++ Tests/ksqlDB.RestApi.Client.IntegrationTests/KSql/RestApi/QueryType.cs
@@ -0,0 +1,8 @@
+namespace ksqlDb.RestApi.Client.IntegrationTests.KSql.RestApi
+{
+  internal static class QueryType
+  {
+    internal static string Push = "PUSH";
+    internal static string Persistent = "PERSISTENT";
+  }
+}
