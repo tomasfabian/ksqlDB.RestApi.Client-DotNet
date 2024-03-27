b26e2050d6b4ed91a3929cf045498826626321b6
Tomas Fabian <t.fabian@cid.com>
2024-03-27 20:46:41 +0100

    [ksqlDb.RestApi.Client]: added docstrings to query response properties

--- ksqlDb.RestApi.Client/KSql/RestApi/Responses/Query/Descriptors/Field.cs
+++ ksqlDb.RestApi.Client/KSql/RestApi/Responses/Query/Descriptors/Field.cs
@@ -2,7 +2,15 @@ namespace ksqlDB.RestApi.Client.KSql.RestApi.Responses.Query.Descriptors;
 
 public record Field
 {
+  /// <summary>
+  /// The name of the field.
+  /// </summary>
   public string Name { get; set; } = null!;
+
+  /// <summary>
+  /// A schema object that describes the schema of the field.
+  /// </summary>
   public Schema Schema { get; set; } = null!;
+  
   public string Type { get; set; } = null!;
 }
--- ksqlDb.RestApi.Client/KSql/RestApi/Responses/Query/Descriptors/QueryDescription.cs
+++ ksqlDb.RestApi.Client/KSql/RestApi/Responses/Query/Descriptors/QueryDescription.cs
@@ -2,18 +2,55 @@ namespace ksqlDB.RestApi.Client.KSql.RestApi.Responses.Query.Descriptors;
 
 public record QueryDescription
 {
+  /// <summary>
+  /// The query ID.
+  /// </summary>
   public string Id { get; set; } = null!;
+
+  /// <summary>
+  /// The ksqlDB statement for which the query being explained is running.
+  /// </summary>
   public string StatementText { get; set; } = null!;
+
   public object? WindowType { get; set; }
+
+  /// <summary>
+  /// An array of field objects that describes each field in the query output.
+  /// </summary>
   public Field[] Fields { get; set; } = null!;
+
+  /// <summary>
+  /// The streams and tables being read by the query.
+  /// </summary>
   public string[] Sources { get; set; } = null!;
+
+  /// <summary>
+  /// The streams and tables being written to by the query.
+  /// </summary>
   public object[] Sinks { get; set; } = null!;
+
+  /// <summary>
+  /// The Kafka Streams topology that the query is running.
+  /// </summary>
   public string Topology { get; set; } = null!;
+
+  /// <summary>
+  /// The query execution plan.
+  /// </summary>
   public string ExecutionPlan { get; set; } = null!;
+
+  /// <summary>
+  /// The property overrides that the query is running with.
+  /// </summary>
   public object? OverriddenProperties { get; set; }
+
   public object? KsqlHostQueryStatus { get; set; }
+
   public string QueryType { get; set; } = null!;
+
   public object[] QueryErrors { get; set; } = null!;
+
   public object[] TasksMetadata { get; set; } = null!;
+
   public object? State { get; set; }
 }
