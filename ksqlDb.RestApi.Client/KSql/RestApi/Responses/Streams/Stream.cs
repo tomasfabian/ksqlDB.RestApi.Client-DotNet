885bde88c553f56acfd3eedc82db58933f39f36b
Tomas Fabian <t.fabian@cid.com>
2024-03-27 20:47:30 +0100

    [ksqlDb.RestApi.Client]: added docstrings to streams response properties

--- ksqlDb.RestApi.Client/KSql/RestApi/Responses/Streams/Stream.cs
+++ ksqlDb.RestApi.Client/KSql/RestApi/Responses/Streams/Stream.cs
@@ -4,18 +4,33 @@ namespace ksqlDB.RestApi.Client.KSql.RestApi.Responses.Streams;
 
 public record Stream
 {
+  /// <summary>
+  /// The source type. Always returns STREAM.
+  /// </summary>
   [JsonPropertyName("type")]
   public string Type { get; set; } = null!;
 
+  /// <summary>
+  /// The name of the stream.
+  /// </summary>
   [JsonPropertyName("name")]
   public string Name { get; set; } = null!;
 
+  /// <summary>
+  /// The topic backing the stream.
+  /// </summary>
   [JsonPropertyName("topic")]
   public string Topic { get; set; } = null!;
 
+  /// <summary>
+  /// The serialization format of the key in the stream. One of JSON, AVRO, PROTOBUF, or DELIMITED.
+  /// </summary>
   [JsonPropertyName("keyFormat")]
   public string KeyFormat { get; set; } = null!;
 
+  /// <summary>
+  /// The serialization format of the data in the stream. One of JSON, AVRO, PROTOBUF, or DELIMITED.
+  /// </summary>
   [JsonPropertyName("valueFormat")]
   public string ValueFormat { get; set; } = null!;
 
--- ksqlDb.RestApi.Client/KSql/RestApi/Responses/Streams/StreamsResponse.cs
+++ ksqlDb.RestApi.Client/KSql/RestApi/Responses/Streams/StreamsResponse.cs
@@ -5,6 +5,9 @@ namespace ksqlDB.RestApi.Client.KSql.RestApi.Responses.Streams;
 
 public record StreamsResponse : StatementResponseBase
 {
+  /// <summary>
+  /// Collection of streams.
+  /// </summary>
   [JsonPropertyName("streams")]
   public Stream[]? Streams { get; set; }
 }
