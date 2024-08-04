11d0aebf1923eabce71063f0b1a8b592a1cc6dae
Tomas Fabian <t.fabian@cid.com>
2024-08-04 12:32:17 +0200

    [ksqlDb.RestApi.Client]: added ServiceCollectionExtensions unit tests

--- Tests/ksqlDB.RestApi.Client.Tests/Infrastructure/Extensions/ServiceCollectionExtensionsTests.cs
+++ Tests/ksqlDB.RestApi.Client.Tests/Infrastructure/Extensions/ServiceCollectionExtensionsTests.cs
@@ -5,12 +5,19 @@ using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using ksqlDB.RestApi.Client.Infrastructure.Extensions;
 using ksqlDB.RestApi.Client.KSql.RestApi;
+using ksqlDB.RestApi.Client.KSql.RestApi.Parameters;
 
 namespace ksqlDb.RestApi.Client.Tests.Infrastructure.Extensions
 {
   public class ServiceCollectionExtensionsTests
   {
-    private readonly IServiceCollection serviceCollection = new ServiceCollection();
+    private IServiceCollection serviceCollection;
+
+    [SetUp]
+    public void Setup()
+    {
+      serviceCollection = new ServiceCollection();
+    }
 
     [Test]
     public void RegisterKSqlDbContextDependencies()
@@ -43,15 +50,59 @@ namespace ksqlDb.RestApi.Client.Tests.Infrastructure.Extensions
     public void ConfigureHttpClients()
     {
       //Arrange
+      var contextOptions = new KSqlDBContextOptions(Helpers.TestParameters.KsqlDbUrl);
 
       //Act
-      serviceCollection.ConfigureHttpClients(new KSqlDBContextOptions(Helpers.TestParameters.KsqlDbUrl));
+      serviceCollection.ConfigureHttpClients(contextOptions);
 
       var serviceProvider = serviceCollection.BuildServiceProvider();
-      var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+      var httpClientFactory = serviceProvider.GetRequiredService<ksqlDB.RestApi.Client.KSql.RestApi.Http.IHttpClientFactory>();
 
       //Assert
       httpClientFactory.Should().NotBeNull();
+
+      var httpClient = httpClientFactory.CreateClient();
+      httpClient.BaseAddress.Should().Be(Helpers.TestParameters.KsqlDbUrl);
+    }
+
+    [Test]
+    public void RegisterEndpointDependencies()
+    {
+      //Arrange
+      var contextOptions = new KSqlDBContextOptions(Helpers.TestParameters.KsqlDbUrl);
+
+      //Act
+      serviceCollection.RegisterEndpointDependencies(contextOptions);
+
+      var serviceProvider = serviceCollection.BuildServiceProvider();
+      var dbParameters = serviceProvider.GetRequiredService<IKSqlDbParameters>();
+      var pullQueryParameters = serviceProvider.GetRequiredService<IPullQueryParameters>();
+
+      //Assert
+      dbParameters.Should().NotBeNull();
+      pullQueryParameters.Should().NotBeNull();
+    }
+
+    [Test]
+    public void ApplyTo()
+    {
+      //Arrange
+      var sc = new ServiceCollection();
+      var restApiClientOptions = new KSqlDBRestApiClientOptions
+      {
+        ShouldPluralizeFromItemName = false,
+      };
+      sc.AddSingleton(restApiClientOptions);
+
+      //Act
+      sc.ApplyTo(serviceCollection);
+
+      var serviceProvider = serviceCollection.BuildServiceProvider();
+      var receivedRestApiClientOptions = serviceProvider.GetRequiredService<KSqlDBRestApiClientOptions>();
+
+      //Assert
+      receivedRestApiClientOptions.Should().NotBeNull();
+      receivedRestApiClientOptions.ShouldPluralizeFromItemName.Should().BeFalse();
     }
   }
 }
